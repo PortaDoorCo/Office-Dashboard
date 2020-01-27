a129780e415f9c3bbadb62e990cae7f3fe6194ab
heyjaypray <hey@jaypray.com>
2020-01-27 15:02:20 -0700

    update permissions

--- src/views/Admin/Products/Products.js
+++ src/views/Admin/Products/Products.js
@@ -22,127 +22,134 @@ const SalesReport = (props) => {
     if (activeTab !== tab) setActiveTab(tab);
   }
 
-  return (
-    <div>
+  const { role } = props;
 
-      <Nav tabs>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
-          >
-            <strong>Menu</strong>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
-          >
-            <strong>Design</strong>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '3' })}
-            onClick={() => { toggle('3'); }}
-          >
-            <strong>Woodtype</strong>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '4' })}
-            onClick={() => { toggle('4'); }}
-          >
-            <strong>Moulds</strong>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '5' })}
-            onClick={() => { toggle('5'); }}
-          >
-            <strong>Edges</strong>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '6' })}
-            onClick={() => { toggle('6'); }}
-          >
-            <strong>Panels</strong>
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '7' })}
-            onClick={() => { toggle('7'); }}
-          >
-            <strong>Finishes</strong>
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <p>Please Select a Product</p>
-        </TabPane>
-        <TabPane tabId="2">
-          <Design
-            product={props.designs}
-            updateProduct={props.updateProduct}
-            addProduct={props.addProduct}
-            getProduct={props.getDesigns}
-            deleteProduct={props.deleteProduct}
-          />
-        </TabPane>
-        <TabPane tabId="3">
-          <Woodtype
-            product={props.woodtypes}
-            updateProduct={props.updateProduct}
-            addProduct={props.addProduct}
-            getProduct={props.getWoodtypes}
-            deleteProduct={props.deleteProduct}
-          />
-        </TabPane>
-        <TabPane tabId="4">
-          <Moulds
-            product={props.moulds}
-            updateProduct={props.updateProduct}
-            addProduct={props.addProduct}
-            getProduct={props.getMoulds}
-            deleteProduct={props.deleteProduct}
-          />
-        </TabPane>
-        <TabPane tabId="5">
-          <Edges
-            product={props.edges}
-            updateProduct={props.updateProduct}
-            addProduct={props.addProduct}
-            getProduct={props.getEdges}
-            deleteProduct={props.deleteProduct}
-          />
-        </TabPane>
-        <TabPane tabId="6">
-          <Panels
-            product={props.panels}
-            updateProduct={props.updateProduct}
-            addProduct={props.addProduct}
-            getProduct={props.getPanels}
-            deleteProduct={props.deleteProduct}
-          />
-        </TabPane>
-        <TabPane tabId="7">
-          <Finishes
-            product={props.finish}
-            updateProduct={props.updateProduct}
-            addProduct={props.addProduct}
-            getProduct={props.getFinish}
-            deleteProduct={props.deleteProduct}
-          />
-        </TabPane>
-      </TabContent>
+  return (
+      role.type === 'management' || role.type === 'root' ?
+        <div>
+
+          <Nav tabs>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '1' })}
+                onClick={() => { toggle('1'); }}
+              >
+                <strong>Menu</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '2' })}
+                onClick={() => { toggle('2'); }}
+              >
+                <strong>Design</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '3' })}
+                onClick={() => { toggle('3'); }}
+              >
+                <strong>Woodtype</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '4' })}
+                onClick={() => { toggle('4'); }}
+              >
+                <strong>Moulds</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '5' })}
+                onClick={() => { toggle('5'); }}
+              >
+                <strong>Edges</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '6' })}
+                onClick={() => { toggle('6'); }}
+              >
+                <strong>Panels</strong>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === '7' })}
+                onClick={() => { toggle('7'); }}
+              >
+                <strong>Finishes</strong>
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              <p>Please Select a Product</p>
+            </TabPane>
+            <TabPane tabId="2">
+              <Design
+                product={props.designs}
+                updateProduct={props.updateProduct}
+                addProduct={props.addProduct}
+                getProduct={props.getDesigns}
+                deleteProduct={props.deleteProduct}
+              />
+            </TabPane>
+            <TabPane tabId="3">
+              <Woodtype
+                product={props.woodtypes}
+                updateProduct={props.updateProduct}
+                addProduct={props.addProduct}
+                getProduct={props.getWoodtypes}
+                deleteProduct={props.deleteProduct}
+              />
+            </TabPane>
+            <TabPane tabId="4">
+              <Moulds
+                product={props.moulds}
+                updateProduct={props.updateProduct}
+                addProduct={props.addProduct}
+                getProduct={props.getMoulds}
+                deleteProduct={props.deleteProduct}
+              />
+            </TabPane>
+            <TabPane tabId="5">
+              <Edges
+                product={props.edges}
+                updateProduct={props.updateProduct}
+                addProduct={props.addProduct}
+                getProduct={props.getEdges}
+                deleteProduct={props.deleteProduct}
+              />
+            </TabPane>
+            <TabPane tabId="6">
+              <Panels
+                product={props.panels}
+                updateProduct={props.updateProduct}
+                addProduct={props.addProduct}
+                getProduct={props.getPanels}
+                deleteProduct={props.deleteProduct}
+              />
+            </TabPane>
+            <TabPane tabId="7">
+              <Finishes
+                product={props.finish}
+                updateProduct={props.updateProduct}
+                addProduct={props.addProduct}
+                getProduct={props.getFinish}
+                deleteProduct={props.deleteProduct}
+              />
+            </TabPane>
+          </TabContent>
+        </div> :
+        <div>
+          Restricted Access
     </div>
+    
   );
 }
 
@@ -154,7 +161,8 @@ const mapStateToProps = (state, prop) => ({
   panels: state.part_list.panels,
   finish: state.part_list.finish,
   file: state.part_list.file,
-  photoId: state.part_list.photoId
+  photoId: state.part_list.photoId,
+  role: state.users.user.role
 });
 
 const mapDispatchToProps = dispatch =>
