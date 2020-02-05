632502c6aafea5863593917ee08d13ce1ea74817
heyjaypray <hey@jaypray.com>
2020-02-05 11:13:39 -0700

    update tracking for restricted tables

--- src/views/Admin/Dashboard/components/RestrictedOrderTable.js
+++ src/views/Admin/Dashboard/components/RestrictedOrderTable.js
@@ -164,7 +164,8 @@ class RestrictedOrderTable extends React.Component {
                         itemPrice: x.itemPrice,
                         subTotals: x.subTotals,
                         tax: x.tax,
-                        files: x.files
+                        files: x.files,
+                        tracking: x.tracking
                     },
                 ],
             });
