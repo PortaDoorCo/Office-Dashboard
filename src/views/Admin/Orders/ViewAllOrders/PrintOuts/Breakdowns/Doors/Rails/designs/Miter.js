e9258bb135ab200909f1c924f10c04dbee02dfa3
heyjaypray <hey@jaypray.com>
2020-03-01 10:33:06 -0500

    update breakdown files

---
+++ src/views/Admin/Orders/ViewAllOrders/PrintOuts/Breakdowns/Doors/Rails/designs/Miter.js
@@ -0,0 +1,112 @@
+import numQty from 'numeric-quantity';
+import Ratio from 'lb-ratio';
+
+const fraction = num => {
+    let fraction = Ratio.parse(num).toQuantityOf(2, 3, 4, 8, 16);
+    return fraction.toLocaleString();
+};
+
+
+export default (info, part) => {
+
+    const add_len = part.design.S_ADD_LEN;
+    const topRail = numQty(info.topRail);
+    const bottomRail = numQty(info.bottomRail);
+    const leftStile = numQty(info.leftStile);
+    const rightStile = numQty(info.rightStile);
+    const horizMull = numQty(info.horizontalMidRailSize);
+
+    if (info.topRail === info.bottomRail) {
+        if ((info.panelsH && info.panelsW > 1) || (info.panelsW && info.panelsH > 1)) {
+          return [
+            {
+              qty: info.qty * 2,
+              measurement: `${fraction(
+                numQty(info.topRail)
+              )} x ${fraction(
+                numQty(info.width) + add_len
+              )}`,
+              pattern: 'TB'
+            },
+            {
+              qty: `${(info.panelsW > 1 && info.panelsH < 2) ? ((info.panelsW - 1) * info.qty) : (info.panelsH > 1 && info.panelsW < 2) ? ((info.panelsH - 1) * info.qty) : (parseInt(info.panelsH) - 1) * info.qty}`,
+              measurement: `${fraction(horizMull)} x ${fraction(
+                numQty(info.width) +
+                part.design.S_ADD_LEN -
+                numQty(leftStile) -
+                numQty(rightStile) +
+                part.design.TENON
+              )}`,
+              pattern: "HM"
+            }
+          ];
+        } else {
+          return [
+            {
+              qty: info.qty * 2,
+              measurement: `${fraction(
+                numQty(info.topRail)
+              )} x ${fraction(
+                numQty(info.width) + add_len
+              )}`,
+              pattern: 'TB'
+            }
+          ];
+        }
+      } else {
+        if ((info.panelsH && info.panelsW > 1) || (info.panelsH > 1 && info.panelsW)) {
+          return [
+            {
+              qty: info.qty,
+              measurement: `${fraction(
+                topRail
+              )} x ${fraction(
+                numQty(info.width) + add_len
+              )}`,
+              pattern: "T"
+            },
+            {
+              qty: info.qty,
+              measurement: `${fraction(
+                bottomRail
+              )} x ${fraction(
+                numQty(info.width) + add_len
+              )}`,
+              pattern: "B"
+            },
+            {
+              qty: `${(info.panelsW > 1 && info.panelsH < 2) ? ((info.panelsW - 1) * info.qty) : (info.panelsH > 1 && info.panelsW < 2) ? ((info.panelsH - 1) * info.qty) : (parseInt(info.panelsH) - 1) * info.qty}`,
+              measurement: `${fraction(horizMull)} x ${fraction(
+                numQty(info.width) +
+                part.design.S_ADD_LEN -
+                numQty(leftStile) -
+                numQty(rightStile) +
+                part.design.TENON
+              )}`,
+              pattern: "HM"
+            }
+          ];
+        } else {
+          return [
+            {
+              qty: info.qty,
+              measurement: `${fraction(
+                topRail
+              )} x ${fraction(
+                numQty(info.width) + add_len
+              )}`,
+              pattern: "T"
+            },
+            {
+              qty: info.qty,
+              measurement: `${fraction(
+                bottomRail
+              )} x ${fraction(
+                numQty(info.width) + add_len
+              )}`,
+              pattern: "B"
+            },
+          ];
+        }
+      }
+}
