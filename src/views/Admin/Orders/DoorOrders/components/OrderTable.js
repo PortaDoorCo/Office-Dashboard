0386037ceb678226a0d8e4c9408248e978bbb571
heyjaypray <hey@jaypray.com>
2020-03-10 09:26:05 -0700

    add lite tables

--- src/views/Admin/Orders/DoorOrders/components/OrderTable.js
+++ src/views/Admin/Orders/DoorOrders/components/OrderTable.js
@@ -15,6 +15,8 @@ import Ratio from "lb-ratio";
 import Maker from '../components/MakerJS/Maker';
 import DropdownList from 'react-widgets/lib/DropdownList';
 import Multiselect from 'react-widgets/lib/Multiselect'
+import PanelsTable from './Table/Table'
+import GlassTable from './Table/Glass'
 
 
 const required = value => (value ? undefined : 'Required');
@@ -183,173 +185,43 @@ const OrderTable = ({ fields, formState, i, prices, subTotal, part, updateSubmit
         <Fragment>
           {fields.map((table, index) => (
             <Fragment key={index}>
-              <Table>
 
-                <Field
-                  name={`${table}.item`}
-                  type="text"
-                  component={renderFieldDisabled}
-                  label="item"
+              {formState.part_list[i].panels && formState.part_list[i].panels.PANEL === "GLASS" ?
+                <GlassTable
+                  table={table}
+                  index={index}
+                  renderField={renderField}
+                  renderFieldDisabled={renderFieldDisabled}
+                  renderMultiSelect={renderMultiSelect}
+                  renderDropdownList={renderDropdownList}
+                  required={required}
+                  w={w}
+                  formState={formState}
+                  i={i}
+                  h={h}
+                  prices={prices}
+                  fields={fields}
+                  doorOptions={doorOptions}
                 />
+                :
+                <PanelsTable
+                  table={table}
+                  index={index}
+                  renderField={renderField}
+                  renderFieldDisabled={renderFieldDisabled}
+                  required={required}
+                  w={w}
+                  formState={formState}
+                  i={i}
+                  h={h}
+                  prices={prices}
+                  fields={fields}
+                />
+              }
 
-                <thead>
-                  <tr>
-                    <th>Qty</th>
-                    <th>Width</th>
-                    <th>Height</th>
-                    <th>Panel High</th>
-                    <th>Panels Wide</th>
-                    <th>Price</th>
-                    <th />
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <Field
-                        name={`${table}.qty`}
-                        type="text"
-                        component={renderField}
-                        label="qty"
-                        validate={required}
-                      />
-                    </td>
-                    <td>
-                      <Field
-                        name={`${table}.width`}
-                        type="text"
-                        component={renderField}
-                        onBlur={e => w(e, formState.part_list[i].dimensions[index].width, index)}
-                        label="width"
-                        validate={required}
-                      />
-                    </td>
 
-                    <td>
-                      <Field
-                        name={`${table}.height`}
-                        type="text"
-                        component={renderField}
-                        onBlur={e => h(e, formState.part_list[i].dimensions[index].height, index)}
-                        label="height"
-                        validate={required}
-                      />
-                    </td>
 
-                    <td>
-                      <Field
-                        name={`${table}.panelsH`}
-                        type="text"
-                        component={renderField}
-                        label="horizontalMidRail"
-                      />
-                    </td>
-                    <td>
-                      <Field
-                        name={`${table}.panelsW`}
-                        type="text"
-                        component={renderField}
-                        label="verticalMidRail"
-                      />
-                    </td>
-                    <td>
-                      {prices[i] ?
-                        <Input
-                          type="text"
-                          className="form-control"
-                          placeholder={"$" + prices[i][index].toFixed(2) || 0}
-                        /> : <Input
-                          type="text"
-                          className="form-control"
-                          placeholder={"$0.00"}
-                        />
-                      }
 
-                    </td>
-                    <td>
-                      <Button color="danger" className="btn-circle" onClick={() => fields.remove(index)}>
-                        X
-                  </Button>
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <td>
-                      <strong>
-                        <p>Left Stile</p>
-                      </strong>
-                      <Field
-                        name={`${table}.leftStile`}
-                        type="text"
-                        component={renderField}
-                        label="leftStile"
-                      />
-                    </td>
-                    <td>
-                      <strong>
-                        <p>Right Stile</p>
-                      </strong>
-                      <Field
-                        name={`${table}.rightStile`}
-                        type="text"
-                        component={renderField}
-                        label="rightStile"
-
-                      />
-                    </td>
-                    <td>
-                      <strong>
-                        <p>Top Rail</p>
-                      </strong>
-                      <Field
-                        name={`${table}.topRail`}
-                        type="text"
-                        component={renderField}
-                        label="topRail"
-
-                      />
-                    </td>
-                    <td>
-                      <strong>
-                        <p>Bottom Rail</p>
-                      </strong>
-                      <Field
-                        name={`${table}.bottomRail`}
-                        type="text"
-                        component={renderField}
-                        label="bottomRail"
-                      />
-                    </td>
-                    <td>
-                      <strong>
-                        <p>Hori. Mid Rail</p>
-                      </strong>
-                      <Field
-                        name={`${table}.horizontalMidRailSize`}
-                        type="text"
-                        component={renderField}
-                        label="horizontalMidRail"
-                      />
-                    </td>
-                    <td>
-                      <strong>
-                        <p>Vert. Mid Rail</p>
-                      </strong>
-                      <Field
-                        name={`${table}.verticalMidRailSize`}
-                        type="text"
-                        component={renderField}
-                        label="verticalMidRail"
-                      />
-                    </td>
-                  </tr>
-                  <Row>
-                    <p className="ml-3">*Finish Stile/Rail Sizes*</p>
-                  </Row>
-                  <tr />
-                </tbody>
-
-              </Table>
               <Row>
                 <Col lg='9'>
                   {(height[index] > 0) ?
@@ -402,20 +274,6 @@ const OrderTable = ({ fields, formState, i, prices, subTotal, part, updateSubmit
                   </Row>
                 </div>
                 : null
-              }
-
-              {formState.part_list[i].panels && formState.part_list[i].panels.PANEL === "GLASS" ?
-                <Row className='mt-2'>
-                  <Col xs="4">
-                    <Label for="jobNotes">Lites</Label>
-                    <Field
-                      name={`${part}.doorOptions`}
-                      component={renderMultiSelect}
-                      data={doorOptions}
-                      textField="option"
-                    />
-                  </Col>
-                </Row> : null
               }
 
               <Row>
