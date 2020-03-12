a5e2c5548eb4fdc5399eba046416769efecba2ad
heyjaypray <hey@jaypray.com>
2020-03-11 21:47:35 -0700

    update

--- src/views/Admin/Orders/ViewAllOrders/PrintOuts/Breakdowns/Doors/Panels.js
+++ src/views/Admin/Orders/ViewAllOrders/PrintOuts/Breakdowns/Doors/Panels.js
@@ -92,8 +92,8 @@ export default (info, part) => {
   const none = [
     {
       qty: ``,
-      measurement: ``,
-      pattern: `GLASS \n ${lites}`
+      measurement: `GLASS \n ${lites}`,
+      pattern: ``
     },
   ]
 
