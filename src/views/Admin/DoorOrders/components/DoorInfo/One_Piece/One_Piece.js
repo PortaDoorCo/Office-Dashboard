8a1b3b1547cd4430bee37cef3eb3d8ee67506f7b
heyjaypray <hey@jaypray.com>
2020-03-19 13:57:27 -0700

    one piece table

--- src/views/Admin/DoorOrders/components/DoorInfo/One_Piece/One_Piece.js
+++ src/views/Admin/DoorOrders/components/DoorInfo/One_Piece/One_Piece.js
@@ -8,11 +8,12 @@ import {
     Button,
     Input
 } from "reactstrap";
-import { Field } from "redux-form";
+import { Field, FieldArray } from "redux-form";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Cookies from "js-cookie";
 import { renderMultiSelect, renderDropdownList, renderDropdownListFilter, renderField } from '../../RenderInputs/renderInputs'
+import One_Piece_Table from '../../Table/Doors/One_Piece_Table'
 
 const required = value => (value ? undefined : 'Required');
 
@@ -31,6 +32,11 @@ class One_Piece_Door extends Component {
             one_piece_edges,
             one_piece_panels,
             finishes,
+
+            isValid,
+            index,
+            part_list,
+            formState,
         } = this.props;
         return (
             <div>
@@ -110,6 +116,38 @@ class One_Piece_Door extends Component {
                     </Col>
 
                 </Row>
+
+                <Row className="mt-2">
+                    <Col xs="4">
+                        <FormGroup>
+                            <strong>
+                                <Label for="jobNotes">Job Notes</Label>
+                                <Field
+                                    name={`${part}.notes`}
+                                    type="textarea"
+                                    component={renderField}
+                                />
+                            </strong>
+                        </FormGroup>
+                    </Col>
+                </Row>
+
+                <div>
+                    <CardSubtitle className="mt-4 mb-1">Dimensions</CardSubtitle>
+                    <div className="mt-1" />
+                    <FieldArray
+                        name={`${part}.dimensions`}
+                        component={One_Piece_Table}
+                        i={index}
+                        // prices={prices}
+                        // subTotal={subTotal}
+                        part_list={part_list}
+                        formState={formState}
+                        isValid={isValid}
+                        part={part}
+                    // updateSubmit={updateSubmit}
+                    />
+                </div>
             </div>
         );
     }
