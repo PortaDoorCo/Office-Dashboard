a5cd5fbfa47a08efb5b557da80a1865559a5dc21
heyjaypray <hey@jaypray.com>
2020-03-19 14:57:36 -0700

    slab door table

--- src/views/Admin/DoorOrders/components/DoorInfo/Slab_Door/Slab_Door.js
+++ src/views/Admin/DoorOrders/components/DoorInfo/Slab_Door/Slab_Door.js
@@ -8,11 +8,12 @@ import {
   Button,
   Input
 } from "reactstrap";
-import { Field } from "redux-form";
+import { Field, FieldArray } from "redux-form";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Cookies from "js-cookie";
 import { renderMultiSelect, renderDropdownList, renderDropdownListFilter, renderField } from '../../RenderInputs/renderInputs'
+import Slab_Door_Table from '../../Table/Doors/Slab_Door_Table'
 
 const required = value => (value ? undefined : 'Required');
 
@@ -30,6 +31,11 @@ class Slab_Door extends Component {
       edges,
       applied_profiles,
       finishes,
+
+      isValid,
+      index,
+      part_list,
+      formState,
     } = this.props;
     return (
       <div>
@@ -79,7 +85,7 @@ class Slab_Door extends Component {
 
         <Row>
 
-        <Col xs="6">
+          <Col xs="6">
             <FormGroup>
               <Label htmlFor="hinges">Applied Profiles</Label>
               <Field
@@ -109,7 +115,39 @@ class Slab_Door extends Component {
           </Col>
 
         </Row>
-      </div>
+
+        <Row className="mt-2">
+          <Col xs="4">
+            <FormGroup>
+              <strong>
+                <Label for="jobNotes">Job Notes</Label>
+                <Field
+                  name={`${part}.notes`}
+                  type="textarea"
+                  component={renderField}
+                />
+              </strong>
+            </FormGroup>
+          </Col>
+        </Row>
+
+        <div>
+          <CardSubtitle className="mt-4 mb-1">Dimensions</CardSubtitle>
+          <div className="mt-1" />
+          <FieldArray
+            name={`${part}.dimensions`}
+            component={Slab_Door_Table}
+            i={index}
+            // prices={prices}
+            // subTotal={subTotal}
+            part_list={part_list}
+            formState={formState}
+            isValid={isValid}
+            part={part}
+          // updateSubmit={updateSubmit}
+          />
+        </div>
+      </div >
     );
   }
 }
