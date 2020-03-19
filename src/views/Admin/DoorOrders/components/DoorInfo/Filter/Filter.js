79c72963809b5d55cdd6ef4546561e00ad806e91
heyjaypray <hey@jaypray.com>
2020-03-18 20:24:14 -0700

    conditional filter

--- src/views/Admin/DoorOrders/components/DoorInfo/Filter/Filter.js
+++ src/views/Admin/DoorOrders/components/DoorInfo/Filter/Filter.js
@@ -1,12 +1,12 @@
 import React, { useState, Fragment, useEffect } from "react";
 import {
-    Row,
-    Col,
-    CardSubtitle,
-    FormGroup,
-    Label,
-    Button,
-    Input
+  Row,
+  Col,
+  CardSubtitle,
+  FormGroup,
+  Label,
+  Button,
+  Input
 } from "reactstrap";
 import { Field } from "redux-form";
 import { renderMultiSelect, renderDropdownList, renderDropdownListFilter, renderField } from '../../RenderInputs/renderInputs'
@@ -19,55 +19,121 @@ const required = value => (value ? undefined : 'Required');
 
 
 
-const DoorFilter = ({ part, construction, thickness, orderType }) => {
+const DoorFilter = ({ formState, index, part, construction, thickness, orderType }) => {
 
-    return (
+  if(formState && formState.part_list){
+    if ((formState.part_list[index].orderType.value === "Door") || (formState.part_list[index].orderType.value === "DF")) {
+      return (
         <Fragment>
-            <Row>
-              <Col xs="4">
-                <FormGroup>
-                  <Label for="orderType">Order Type</Label>
-                  <Field
-                    name={`${part}.orderType`}
-                    component={renderDropdownList}
-                    data={orderType}
-                    valueField="value"
-                    textField="name"
-                    validate={required}
-                  />
-                </FormGroup>
-              </Col>
-
-              <Col xs="4">
-                <FormGroup>
-                  <Label for="construction">Construction</Label>
-                  <Field
-                    name={`${part}.construction`}
-                    component={renderDropdownList}
-                    data={construction}
-                    valueField="value"
-                    textField="name"
-                    validate={required}
-                  />
-                </FormGroup>
-              </Col>
-
-              <Col xs="4">
-                <FormGroup>
-                  <Label for="construction">Thickness</Label>
-                  <Field
-                    name={`${part}.thickness`}
-                    component={renderDropdownList}
-                    data={thickness}
-                    valueField="value"
-                    textField="name"
-                    validate={required}
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
+          <Row>
+            <Col xs="4">
+              <FormGroup>
+                <Label for="orderType">Order Type</Label>
+                <Field
+                  name={`${part}.orderType`}
+                  component={renderDropdownList}
+                  data={orderType}
+                  valueField="value"
+                  textField="name"
+                  validate={required}
+                />
+              </FormGroup>
+            </Col>
+  
+            <Col xs="4">
+              <FormGroup>
+                <Label for="construction">Construction</Label>
+                <Field
+                  name={`${part}.construction`}
+                  component={renderDropdownList}
+                  data={construction}
+                  valueField="value"
+                  textField="name"
+                  validate={required}
+                />
+              </FormGroup>
+            </Col>
+  
+            <Col xs="4">
+              <FormGroup>
+                <Label for="construction">Thickness</Label>
+                <Field
+                  name={`${part}.thickness`}
+                  component={renderDropdownList}
+                  data={thickness}
+                  valueField="value"
+                  textField="name"
+                  validate={required}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
         </Fragment>
-    )
+      )
+    } 
+    if (formState.part_list[index].orderType.value === "Frame_Only") {
+      return (
+        <Fragment>
+          <Row>
+            <Col xs="4">
+              <FormGroup>
+                <Label for="orderType">Order Type</Label>
+                <Field
+                  name={`${part}.orderType`}
+                  component={renderDropdownList}
+                  data={orderType}
+                  valueField="value"
+                  textField="name"
+                  validate={required}
+                />
+              </FormGroup>
+            </Col>
+ 
+            <Col xs="4">
+              <FormGroup>
+                <Label for="construction">Thickness</Label>
+                <Field
+                  name={`${part}.thickness`}
+                  component={renderDropdownList}
+                  data={thickness}
+                  valueField="value"
+                  textField="name"
+                  validate={required}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Fragment>
+      )
+    } else {
+      return (
+        <Fragment>
+          <Row>
+            <Col xs="4">
+              <FormGroup>
+                <Label for="orderType">Order Type</Label>
+                <Field
+                  name={`${part}.orderType`}
+                  component={renderDropdownList}
+                  data={orderType}
+                  valueField="value"
+                  textField="name"
+                  validate={required}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Fragment>
+      )
+    }
+
+  } else {
+    return null
+  }
+
+
+
+
 }
 
 export default DoorFilter;
