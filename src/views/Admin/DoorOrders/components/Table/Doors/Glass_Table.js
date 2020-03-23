b32d2bfabea03a7fe05c54194bf39cb0a3ea438e
heyjaypray <hey@jaypray.com>
2020-03-23 11:15:29 -0700

    add glass / one piece pricing

--- src/views/Admin/DoorOrders/components/Table/Doors/Glass_Table.js
+++ src/views/Admin/DoorOrders/components/Table/Doors/Glass_Table.js
@@ -153,22 +153,22 @@ const Cope_Table = ({ fields, formState, i, prices, subTotal, part, updateSubmit
                       />
                     </td>
                     <td>
-                      {/* {prices[i] ?
-                <Input
-                    type="text"
-                    className="form-control"
-                    // placeholder={"$" + prices[i][index].toFixed(2) || 0}
-                /> : */}
-                      <Input
-                        type="text"
-                        className="form-control"
-                        placeholder={"$0.00"}
-                      />
-                      {/* } */}
+                      {prices[i] ?
+                        <Input
+                          type="text"
+                          className="form-control"
+                          placeholder={"$" + prices[i][index].toFixed(2) || 0}
+                        /> :
+                        <Input
+                          type="text"
+                          className="form-control"
+                          placeholder={"$0.00"}
+                        />
+                      }
 
                     </td>
                     <td>
-                        <Button color="danger" className="btn-circle" onClick={() => fields.remove(index)}>
+                      <Button color="danger" className="btn-circle" onClick={() => fields.remove(index)}>
                         X
                         </Button>
                     </td>
