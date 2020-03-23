7b9f962324e11a824e4582fd4aac37743db2bed6
heyjaypray <hey@jaypray.com>
2020-03-23 13:21:46 -0700

    face frame pricing

--- src/selectors/doorPricing.js
+++ src/selectors/doorPricing.js
@@ -85,28 +85,58 @@ export const linePriceSelector = createSelector(
       const applied_profile = part.applied_profile ? part.applied_profile.UPCHARGE : 0;
       const finish = part.finish ? part.finish.UPCHARGE : 0;
       const lites = part.lites ? part.lites.UPCHARGE : 0
+      const ff_opening_cost = part.design ? part.design.opening_cost : 0
+      const ff_top_rail_design = part.face_frame_top_rail ? part.face_frame_top_rail.UPCHARGE : 0
+      const furniture_feet = part.furniture_feet ? part.furniture_feet.UPCHARGE : 0
+      const ff_design = part.design ? part.design.opening_cost : 0
 
 
 
-      if (part.dimensions) {
-        const linePrice = part.dimensions.map(i => {
-          let widths = numQty(i.width);
-          let heights = numQty(i.height);
+      if (part.orderType.value === "Face_Frame") {
+        if (part.dimensions) {
+          const linePrice = part.dimensions.map(i => {
+            console.log(i)
+            let widths = numQty(i.width);
+            let heights = numQty(i.height);
+            let openings = parseInt(i.openings)
 
-          const price =
-            ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
-              (design + edge + panel + applied_profile + finish + lites)) *
-            parseInt(i.qty) || 0;
+            const price =
+              ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
+                (((openings * ff_opening_cost)*ff_top_rail_design) + (furniture_feet + edge))) *
+              parseInt(i.qty) || 0;
 
-          if (heights > -1) {
-            return price;
-          } else {
-            return 0;
-          }
-        });
-        return linePrice;
+            if (heights > -1) {
+              return price;
+            } else {
+              return 0;
+            }
+          });
+          console.log(linePrice)
+          return linePrice;
+        } else {
+          return 0;
+        }
       } else {
-        return 0;
+        if (part.dimensions) {
+          const linePrice = part.dimensions.map(i => {
+            let widths = numQty(i.width);
+            let heights = numQty(i.height);
+
+            const price =
+              ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
+                (design + edge + panel + applied_profile + finish + lites)) *
+              parseInt(i.qty) || 0;
+
+            if (heights > -1) {
+              return price;
+            } else {
+              return 0;
+            }
+          });
+          return linePrice;
+        } else {
+          return 0;
+        }
       }
     })
 );
