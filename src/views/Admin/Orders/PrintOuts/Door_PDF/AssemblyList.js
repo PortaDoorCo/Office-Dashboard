b6de76baa0d6a2e4b63c79240ae97ac1c50c3be1
heyjaypray <hey@jaypray.com>
2020-03-25 12:24:12 -0700

    update breakdowns

--- src/views/Admin/Orders/PrintOuts/Door_PDF/AssemblyList.js
+++ src/views/Admin/Orders/PrintOuts/Door_PDF/AssemblyList.js
@@ -13,7 +13,7 @@ export default data => {
           stack: [
 
             { text: 'Assembly List', bold: true },
-            `Shipping Date: ${moment(data.jobInfo.DueDate).format('MM/DD/YYYY')}`,
+            `Shipping Date: ${moment(data.job_info.DueDate).format('MM/DD/YYYY')}`,
             { qr: `${data.id}`, fit: '75', margin: [0, 5, 0, 0] },
           ]
         },
@@ -30,7 +30,7 @@ export default data => {
         {
           stack: [
             { text: `Order #: ${data.orderNum}`, alignment: 'right' },
-            { text: `Est. Ship: ${moment(data.jobInfo.DueDate).format('MM/DD/YYYY')}`, alignment: 'right' }
+            { text: `Est. Ship: ${moment(data.job_info.DueDate).format('MM/DD/YYYY')}`, alignment: 'right' }
           ]
         }
       ]
@@ -38,7 +38,7 @@ export default data => {
     {
       columns: [
         {
-          text: `${data.jobInfo.jobName} - ${data.jobInfo.customer.Company}`,
+          text: `${data.job_info.jobName} - ${data.job_info.customer.Company}`,
           margin: [0, 10]
         },
         { text: 'Job: None', alignment: 'right', margin: [0, 0, 80, 0] }
