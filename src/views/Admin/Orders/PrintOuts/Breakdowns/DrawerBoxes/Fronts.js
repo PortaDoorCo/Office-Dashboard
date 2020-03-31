c4765f7a0b426172252eff47c6ed2172727a372a
heyjaypray <hey@jaypray.com>
2020-03-31 11:23:24 -0700

    drawer breakdowns

--- src/views/Admin/Orders/PrintOuts/Breakdowns/DrawerBoxes/Fronts.js
+++ src/views/Admin/Orders/PrintOuts/Breakdowns/DrawerBoxes/Fronts.js
@@ -3,7 +3,7 @@ export default (item, part) => {
 
     return {
         qty: (parseInt(item.qty) * 2),
-        measurement: `${item.height} x ${item.width} x ${part.boxThickness.Thickness}`,
+        measurement: `${item.height} x ${item.width} x ${part.boxThickness.NAME}`,
         pattern: "Fronts/Backs"
     }
 };
