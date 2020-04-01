2f9fd4ab6edec02ab66308ba4dd02189119cc16a
heyjaypray <hey@jaypray.com>
2020-03-31 20:42:42 -0700

    update balance on print outs

--- src/views/Admin/Orders/PrintOuts/Door_PDF/Acknowledgement.js
+++ src/views/Admin/Orders/PrintOuts/Door_PDF/Acknowledgement.js
@@ -13,6 +13,11 @@ export default data => {
 
 
   const subTotal = data.subTotals.reduce((acc, item) => acc + item, 0)
+  const balanceDue = data.balance_history[data.balance_history.length - 1].balance_due;
+  const balancePaid = data.balance_history.reduce(function (accumulator, balance) {
+    return accumulator + balance.balance_paid;
+  }, 0);
+
 
 
 
@@ -166,7 +171,7 @@ export default data => {
       columns: [
         { text: '', style: 'totals', width: 347 },
         { text: 'Balance Paid:', style: 'totals', margin: [0, 0, 0, 0] },
-        { text: `$${(data.balance_paid).toFixed(2)}`, style: 'fonts', margin: [0, 0, 0, 0], alignment: 'right' }
+        { text: `$${(balancePaid).toFixed(2)}`, style: 'fonts', margin: [0, 0, 0, 0], alignment: 'right' }
       ],
       margin: [0, 2, 0, 0],
 
@@ -175,7 +180,7 @@ export default data => {
       columns: [
         { text: '', style: 'totals', width: 347 },
         { text: 'Balance Due:', style: 'totals', margin: [0, 0, 0, 0] },
-        { text: `$${(data.balance_due).toFixed(2)}`, style: 'fonts', margin: [0, 0, 0, 0], alignment: 'right' }
+        { text: `$${(balanceDue).toFixed(2)}`, style: 'fonts', margin: [0, 0, 0, 0], alignment: 'right' }
       ],
       margin: [0, 15, 0, 0]
     },
