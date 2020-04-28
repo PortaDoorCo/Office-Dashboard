d8a2df580d0bcf1b6a219dfe48597353b072430c
heyjaypray <hey@jaypray.com>
2020-04-28 13:42:08 -0700

    add eval to cope breakdowns

--- src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/Rails/Rails.js
+++ src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/Rails/Rails.js
@@ -12,11 +12,13 @@ import Slab_Door from './designs/Slab_Door/Slab_Door'
 
 
 
-export default (info, part) => {
+export default (info, part, breakdowns) => {
+
+  console.log("SKLDFUFUCKKKK", breakdowns)
 
   if(part.orderType.value === "Door") {
     if (part.construction.value === "Cope") {
-      return Cope_Door(info, part)
+      return Cope_Door(info, part, breakdowns[0])
     }
     if (part.construction.value === "M") {
       return Miter_Door(info, part)
