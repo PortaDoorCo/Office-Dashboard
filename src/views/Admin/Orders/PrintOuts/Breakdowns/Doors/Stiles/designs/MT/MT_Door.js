0207151e7f0555de71e2af77fb0b813ff9f09b90
heyjaypray <hey@jaypray.com>
2020-05-01 21:40:28 -0700

    quick fix

--- src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/Stiles/designs/MT/MT_Door.js
+++ src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/Stiles/designs/MT/MT_Door.js
@@ -31,8 +31,11 @@ export default (info, part, breakdowns) => {
   const add_len = 0;
   const tenon_factor = 0;
 
+  console.log(eval(breakdowns.topRail_width))
+  console.log(eval(breakdowns.bottomRail_width))
 
-  if (eval(breakdowns.topRail_width) === eval(breakdowns.bottomRail_width)) {
+
+  if (eval(breakdowns.leftStile_width) === eval(breakdowns.rightStile_width)) {
     if (((panelsW > 1) && (panelsH > 1)) || ((panelsW > 1) && (panelsH == 1))) {
       return [
         {
