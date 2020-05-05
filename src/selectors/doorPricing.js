9da781e7c5150619e04f7074b1e41e1634911846
heyjaypray <hey@jaypray.com>
2020-05-04 17:41:36 -0700

    update

--- src/selectors/doorPricing.js
+++ src/selectors/doorPricing.js
@@ -77,8 +77,77 @@ export const itemPriceSelector = createSelector(
   (parts) =>
     parts.map((part, index) => {
       console.log(part)
-      const wood = part.woodtype ? part.woodtype.STANDARD_GRADE : 0;
-      const design = part.design ? part.design.UPCHARGE : 0;
+      const wood = part.woodtype && part.thickness.value === 0.75 ? part.woodtype.STANDARD_GRADE : (part.woodtype && part.thickness.value === 1) ? part.woodtype.STANDARD_GRADE_THICK :  0;
+      const design = part.design && part.thickness.value === 0.75 ? part.design.UPCHARGE : (part.design && part.thickness.value === 1) ? part.design.UPCHARGE_THICK :  0;
+      const edge = part.edge ? part.edge.UPCHARGE : 0;
+      const panel = part.panel ? part.panel.UPCHARGE : 0;
+      const applied_profile = part.applied_profile ? part.applied_profile.UPCHARGE : 0;
+      const finish = part.finish ? part.finish.UPCHARGE : 0;
+      const lites = part.lites ? part.lites.UPCHARGE : 0
+      const ff_opening_cost = part.design && part.orderType.value === "Face_Frame" ? part.design.opening_cost : 0
+      const ff_top_rail_design = part.face_frame_top_rail ? part.face_frame_top_rail.UPCHARGE : 0
+      const furniture_feet = part.furniture_feet ? part.furniture_feet.UPCHARGE : 0
+
+
+      if (part.orderType.value === "Face_Frame") {
+        if (part.dimensions) {
+          const linePrice = part.dimensions.map(i => {
+            console.log(i)
+            let widths = numQty(i.width);
+            let heights = numQty(i.height);
+            let openings = parseInt(i.openings)
+
+            const price =
+              ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
+                (((openings * ff_opening_cost)*ff_top_rail_design) + (furniture_feet + edge))) *
+              parseInt(i.qty) || 0;
+
+            if (heights > -1) {
+              return price;
+            } else {
+              return 0;
+            }
+          });
+          console.log(linePrice)
+          return linePrice;
+        } else {
+          return 0;
+        }
+      } else {
+        if (part.dimensions) {
+          const linePrice = part.dimensions.map(i => {
+            let widths = numQty(i.width);
+            let heights = numQty(i.height);
+
+            const price =
+              ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
+                (design + edge + panel + applied_profile + finish + lites)) *
+              parseInt(i.qty) || 0;
+
+              console.log('PRICE',price)
+
+            if (heights > -1) {
+              return price;
+            } else {
+              return 0;
+            }
+          });
+          return linePrice;
+        } else {
+          return 0;
+        }
+      }
+    })
+);
+
+
+export const linePriceSelector = createSelector(
+  [partListSelector],
+  (parts) =>
+    parts.map((part, index) => {
+      console.log(part)
+      const wood = part.woodtype && part.thickness.value === 0.75 ? part.woodtype.STANDARD_GRADE : part.woodtype && part.thickness.value === 1 ? part.woodtype.STANDARD_GRADE_THICK :  0;
+      const design = part.design && part.thickness.value === 0.75 ? part.design.UPCHARGE : part.design && part.thickness.value === 1 ? part.design.UPCHARGE_THICK :  0;
       const edge = part.edge ? part.edge.UPCHARGE : 0;
       const panel = part.panel ? part.panel.UPCHARGE : 0;
       const applied_profile = part.applied_profile ? part.applied_profile.UPCHARGE : 0;
@@ -125,73 +194,7 @@ export const itemPriceSelector = createSelector(
                 (design + edge + panel + applied_profile + finish + lites)) *
               parseInt(i.qty) || 0;
 
-            if (heights > -1) {
-              return price;
-            } else {
-              return 0;
-            }
-          });
-          return linePrice;
-        } else {
-          return 0;
-        }
-      }
-    })
-);
-
-
-export const linePriceSelector = createSelector(
-  [partListSelector],
-  (parts) =>
-    parts.map((part, index) => {
-      console.log(part)
-      const wood = part.woodtype && part.thickness.value === 0.75 ? part.woodtype.STANDARD_GRADE : part.woodtype && part.thickness.value === 1 ? part.woodtype.STANDARD_GRADE_THICK :  0;
-      const design = part.design && part.thickness.value === 0.75 ? part.design.UPCHARGE : part.design && part.thickness.value === 1 ? part.design.UPCHARGE_THICK :  0;
-      const edge = part.edge && part.thickness.value === 0.75 ? part.edge.UPCHARGE : part.edge && part.thickness.value === 1 ? part.edge.UPCHARGE_THICK : 0;
-      const panel = part.panel ? part.panel.UPCHARGE : 0;
-      const applied_profile = part.applied_profile ? part.applied_profile.UPCHARGE : 0;
-      const finish = part.finish ? part.finish.UPCHARGE : 0;
-      const lites = part.lites ? part.lites.UPCHARGE : 0
-      const ff_opening_cost = part.design ? part.design.opening_cost : 0
-      const ff_top_rail_design = part.face_frame_top_rail ? part.face_frame_top_rail.UPCHARGE : 0
-      const furniture_feet = part.furniture_feet ? part.furniture_feet.UPCHARGE : 0
-
-
-
-      if (part.orderType.value === "Face_Frame") {
-        if (part.dimensions) {
-          const linePrice = part.dimensions.map(i => {
-            console.log(i)
-            let widths = numQty(i.width);
-            let heights = numQty(i.height);
-            let openings = parseInt(i.openings)
-
-            const price =
-              ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
-                (((openings * ff_opening_cost)*ff_top_rail_design) + (furniture_feet + edge))) *
-              parseInt(i.qty) || 0;
-
-            if (heights > -1) {
-              return price;
-            } else {
-              return 0;
-            }
-          });
-          console.log(linePrice)
-          return linePrice;
-        } else {
-          return 0;
-        }
-      } else {
-        if (part.dimensions) {
-          const linePrice = part.dimensions.map(i => {
-            let widths = numQty(i.width);
-            let heights = numQty(i.height);
-
-            const price =
-              ((((Math.ceil(widths) * Math.ceil(heights)) / 144) * wood) +
-                (design + edge + panel + applied_profile + finish + lites)) *
-              parseInt(i.qty) || 0;
+            console.log('PRICE',price)
 
             if (heights > -1) {
               return price;
