d24a8cbe3ee5de021c098a67a1331cb5ae70bc61
heyjaypray <hey@jaypray.com>
2020-05-12 13:34:32 -0700

    quick fix glass door

--- src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/Size.js
+++ src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/Size.js
@@ -1,3 +1,10 @@
+import Ratio from 'lb-ratio';
+
+const fraction = num => {
+  let fraction = Ratio.parse(num).toQuantityOf(2, 3, 4, 8, 16);
+  return fraction.toLocaleString();
+};
+
 export default item => {
-  return `${item.width} x ${item.height}`;
+  return `${fraction(item.width)} x ${fraction(item.height)}`;
 };
