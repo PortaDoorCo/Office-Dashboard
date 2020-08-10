b1cb053bb89f4be89e38edf6edca79102d87f8f4
heyjaypray <hey@jaypray.com>
2020-08-09 23:32:08 -0400

    update

--- src/components/RenderInputs/RenderPriceHolder.js
+++ src/components/RenderInputs/RenderPriceHolder.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import {
-    Input,
-    InputGroup,
-    InputGroupAddon,
-    InputGroupText
-} from 'reactstrap'
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText
+} from 'reactstrap';
 
 class RenderPriceHolder extends Component {
-    render() {
-        const { edit } = this.props;
-        return (
-            <div>
-                <InputGroup>
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>$</InputGroupText>
-                    </InputGroupAddon>
-                    <Input placeholder={this.props.input} disabled={edit} autoComplete="new-password" />
-                </InputGroup>
-            </div>
-        );
-    }
+  render() {
+    const { edit } = this.props;
+    return (
+      <div>
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>$</InputGroupText>
+          </InputGroupAddon>
+          <Input placeholder={this.props.input} disabled={edit} autoComplete="new-password" />
+        </InputGroup>
+      </div>
+    );
+  }
 }
 
 export default RenderPriceHolder;
