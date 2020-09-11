f129d0e040df16958c45207d069535b7cea936fc
heyjaypray <hey@jaypray.com>
2020-09-11 13:04:34 -0400

    update tests

--- src/components/DoorOrders/SideBar/Glass/__test__/DF.test.js
+++ src/components/DoorOrders/SideBar/Glass/__test__/DF.test.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from '../../../../../../../rootReducer';
+import rootReducer from '../../../../../rootReducer';
 import { reduxForm } from 'redux-form';
 
 const middleware = [thunk];
