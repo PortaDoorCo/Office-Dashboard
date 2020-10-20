023bef4b3ba23168bd32d8747efb0f0d8131d8f2
heyjaypray <hey@jaypray.com>
2020-10-20 11:46:25 -0700

    update mt df designs

--- src/components/DoorOrders/DoorInfo/MT/DF.js
+++ src/components/DoorOrders/DoorInfo/MT/DF.js
@@ -221,7 +221,7 @@ class MT_DF extends Component {
 
 const mapStateToProps = state => ({
   woodtypes: state.part_list.woodtypes,
-  mt_designs: state.part_list.mt_designs,
+  mt_designs: state.part_list.mt_df_designs,
   edges: state.part_list.edges,
   panels: state.part_list.panels,
   profiles: state.part_list.profiles,
