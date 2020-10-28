454f3f1c3f4322df18ba55abe247baaa8bc09d08
heyjaypray <hey@jaypray.com>
2020-10-28 09:20:19 -0700

    fix drawer breakdowns

--- src/views/Admin/Orders/PrintOuts/Breakdowns/DrawerBoxes/Sides.js
+++ src/views/Admin/Orders/PrintOuts/Breakdowns/DrawerBoxes/Sides.js
@@ -1,4 +1,5 @@
 import Ratio from 'lb-ratio';
+import numQty from 'numeric-quantity'
 
 const fraction = num => {
   let fraction = Ratio.parse(num).toQuantityOf(2, 3, 4, 8, 16);
@@ -8,8 +9,9 @@ export default (item, part, breakdowns) => {
 
   const b = breakdowns[0];
 
-  const height = item.height;
-  const depth = parseInt(item.depth);
+  const width = numQty(item.width);
+  const height = numQty(item.height);
+  const depth = numQty(item.depth);
   const sideDeduction = part.box_thickness.SIDE_DEDUCTION;
   const bottomWidthReduction = part.box_thickness.BOTTOM_WIDTH_DEDUCTION;
   const boxThickness = part.box_thickness.NAME;
