8b1f30a0e1319b8411c28dd70eee05c21b394846
heyjaypray <hey@jaypray.com>
2021-02-02 14:57:47 -0700

    update filter on misc items

--- src/components/DrawerOrders/MiscItems.js
+++ src/components/DrawerOrders/MiscItems.js
@@ -49,9 +49,7 @@ let Inputs = (props) => {
 
   let misc_items_category = ['Accessories', 'Drawer_Box'];
 
-  let sorted_misc_items_start = misc_items.map(i => i.categories.filter(j => misc_items_category.includes(j.value)));
-
-  let sorted_misc_items = sorted_misc_items_start.filter(i => i.length > 0);
+  let sorted_misc_items = misc_items.filter(e => e.categories.some(c => misc_items_category.includes(c.value)));
 
   const changeMiscItem = (e, index) => {
 
