740f2ae090b025f7b7bb8bba72d67ec4c11d50a8
heyjaypray <hey@jaypray.com>
2021-02-05 22:12:08 -0700

    update

--- src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/MaterialBreakdown/LinearFT.js
+++ src/views/Admin/Orders/PrintOuts/Breakdowns/Doors/MaterialBreakdown/LinearFT.js
@@ -1,6 +1,7 @@
 import numQty from 'numeric-quantity';
 import { flatten, values } from 'lodash';
 import Stiles from '../Stiles/Stiles';
+import Rails from '../Rails/Rails';
 
 
 export default (parts, breakdowns,thickness) => {
@@ -9,18 +10,93 @@ export default (parts, breakdowns,thickness) => {
 
   const calc = parts.map((part, i) => {
     return part.items.map(j => {
-      console.log(values(j).filter(a => a === thickness));
-      console.log({j});
-      console.log({thickness});
 
-      const stiles = Stiles(j, part.part, breakdowns).map((stile) => {
-        const height = ((numQty(stile.height)) * 2) * parseInt(j.qty);
-        const width = (numQty(stile.width) * 2) * parseInt(j.qty);
-        const sum = (height + width) / 12;
-        return sum;
+      // const filtered = Object.keys(j).reduce(function(r, e) {
+      //   if (thickness.includes(j[e])) r[e] = j[e];
+      //   return r;
+      // }, {});
+
+      let stile = {};
+      let rail = {};
+
+      const stile_map = Object.keys(j).map(a => {
+        if(a === 'leftStile'){
+          return j[a] === thickness ? stile[a] = j[a] : stile[a] = 0;
+        }
+        if(a === 'rightStile'){
+          return j[a] === thickness ? stile[a] = j[a] : stile[a] = 0;
+        }
+        
+        if(a === 'horizontalMidRailSize'){
+          return j[a] === thickness ? rail[a] = j[a] : rail[a] = 0;
+        }
+
+        if(a === 'verticalMidRailSize'){
+          return j[a] === thickness ? stile[a] = j[a] : stile[a] = 0;
+        }
+        else {
+          return stile[a] = j[a];
+        }
+      }, {});
+
+      const rail_map = Object.keys(j).map(a => {
+        if(a === 'topRail'){
+          return j[a] === thickness ? rail[a] = j[a] : rail[a] = 0;
+        }
+        if(a === 'bottomRail'){
+          return j[a] === thickness ? rail[a] = j[a] : rail[a] = 0;
+        }
+
+        if(a === 'horizontalMidRailSize'){
+          return j[a] === thickness ? rail[a] = j[a] : rail[a] = 0;
+        }
+
+        if(a === 'verticalMidRailSize'){
+          return j[a] === thickness ? stile[a] = j[a] : stile[a] = 0;
+        }
+
+        else {
+          return rail[a] = j[a];
+        }
+      }, {});
+
+
+      console.log({stile_map});
+
+      const stiles = Stiles(stile, part.part, breakdowns).map((stile) => {
+        console.log({stile});
+        console.log(j.qty);
+        if((stile.width > 0) && (stile.height > 0)){
+          const width = numQty(stile.width);
+          const height = ((numQty(stile.height)) * stile.multiplier) * parseInt(j.qty);
+          const sum = height / 12;
+          return sum;
+          
+        } else {
+          return 0;
+        }
+
       });
 
-      return stiles.reduce((acc, item) => acc + item, 0);;
+      const rails = Rails(rail, part.part, breakdowns).map((stile) => {
+        console.log({stile});
+        if((stile.width > 0) && (stile.height > 0)){
+          const height = ((numQty(stile.height)) * stile.multiplier) * parseInt(j.qty);
+          const sum = height / 12;
+          return sum;
+        } else {
+          return 0;
+        }
+
+      });
+
+      const stiles_total = stiles.reduce((acc, item) => acc + item, 0);
+      const rails_total = rails.reduce((acc, item) => acc + item, 0);
+
+      console.log({stiles_total});
+      console.log({rails_total});
+
+      return stiles_total + rails_total;
     });
   });
 
