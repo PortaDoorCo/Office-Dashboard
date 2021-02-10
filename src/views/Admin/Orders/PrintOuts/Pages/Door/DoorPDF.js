d875da2d0883dcf40475af7adfa97cca4c1dd7c1
heyjaypray <hey@jaypray.com>
2021-02-10 10:55:42 -0700

    add printer settings

--- src/views/Admin/Orders/PrintOuts/Pages/Door/DoorPDF.js
+++ src/views/Admin/Orders/PrintOuts/Pages/Door/DoorPDF.js
@@ -8,22 +8,60 @@ import MaterialsList from '../../Door_PDF/MaterialsList';
 import QC_Checklist from '../../Door_PDF/QC_Checklist';
 import Profiles from '../../Door_PDF/Profiles';
 
-export default (data, edges, moulds, miter, mt, panels, appliedProfiles, breakdowns) => {
+export default (data, edges, moulds, miter, mt, panels, appliedProfiles, breakdowns, p) => {
   const { vfs } = vfsFonts.pdfMake;
   pdfMake.vfs = vfs;
+
+  console.log({p});
+
+  let Content = [];
+
+  for (let i = 0; i < p.assembly_list; i++) {
+    Content.push(AssemblyList(data, breakdowns));
+  }
+
+  for (let i = 0; i < p.stiles; i++) {
+    Content.push(StilesPage(data, breakdowns));
+  }
+
+  for (let i = 0; i < p.rails; i++) {
+    Content.push(RailsPage(data, breakdowns));
+  }
+
+  for (let i = 0; i < p.panels; i++) {
+    Content.push(PanelsPage(data, breakdowns));
+  }
+
+
+  for (let i = 0; i < p.materials; i++) {
+    Content.push(MaterialsList(data, breakdowns));
+  }
+
+  for (let i = 0; i < p.qc; i++) {
+    Content.push(QC_Checklist(data, breakdowns));
+  }
+
+  
+  for (let i = 0; i < p.profiles; i++) {
+    Content.push(Profiles(data, edges, moulds, miter, mt, panels, appliedProfiles, breakdowns));
+  }
+
+
 
   const documentDefinition = {
     pageSize: 'A4',
     pageOrientation: 'portrait',
-    content: [
-      AssemblyList(data, breakdowns),
-      StilesPage(data, breakdowns),
-      RailsPage(data, breakdowns),
-      PanelsPage(data, breakdowns),
-      MaterialsList(data, breakdowns),
-      QC_Checklist(data, breakdowns),
-      Profiles(data, edges, moulds, miter, mt, panels, appliedProfiles, breakdowns),
-    ],
+    content: Content,
+    // [
+      
+    //   AssemblyList(data, breakdowns),
+    //   StilesPage(data, breakdowns),
+    //   RailsPage(data, breakdowns),
+    //   PanelsPage(data, breakdowns),
+    //   MaterialsList(data, breakdowns),
+    //   QC_Checklist(data, breakdowns),
+    //   Profiles(data, edges, moulds, miter, mt, panels, appliedProfiles, breakdowns),
+    // ],
     styles: {
       woodtype: {
         fontSize: 18,
