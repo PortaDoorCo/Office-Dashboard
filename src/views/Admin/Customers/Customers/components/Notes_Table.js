7ed219ef27bced0cff5780b452069ff9c50ed234
heyjaypray <hey@jaypray.com>
2021-02-16 15:47:40 -0700

    add notification

--- src/views/Admin/Customers/Customers/components/Notes_Table.js
+++ src/views/Admin/Customers/Customers/components/Notes_Table.js
@@ -1,6 +1,16 @@
-import React, { Component } from 'react';
-import {  Button, Table } from 'reactstrap';
-import { updateNotes, deleteNote } from '../../../../../redux/customers/actions';
+import React, { useState } from 'react';
+import {
+  Button,
+  Table,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from 'reactstrap';
+import {
+  updateNotes,
+  deleteNote,
+} from '../../../../../redux/customers/actions';
 import { reduxForm, change, getFormValues, arrayRemove } from 'redux-form';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -9,58 +19,110 @@ import Cookies from 'js-cookie';
 
 const cookie = Cookies.get('jwt');
 
-class Notes_Table extends Component {
-  render() {
+let Notes_Table = props =>  {
 
-    const { formState, deleteNote } = this.props;
+  const { formState, deleteNote, className } = props;
+  const [modal, setModal] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
 
-    if (formState) {
+  console.log({selectedItemId});
+  console.log({selectedItemIndex});
 
-      return (
-        <div>
-          <Table striped>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Notes</th>
-                <th>Submitted By:</th>
-                <th />
 
-              </tr>
-            </thead>
-            <tbody>
-              {(formState && formState.Customer_Notes) ? formState.Customer_Notes.reverse().map((i, index) => (
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  const del = (id, index) => {
+
+    console.log({id});
+    console.log({index});
+
+
+    props.dispatch(
+      arrayRemove(
+        'CustomerEdit',
+        'Customer_Notes',
+        index
+      )
+    );
+    deleteNote(id, formState, cookie);
+    toggle();
+    
+
+  };
+
+  if (formState) {
+    return (
+      <div>
+        <Modal isOpen={modal} toggle={toggle} className={className}>
+          <ModalHeader toggle={toggle}>Are You Sure?</ModalHeader>
+          <ModalBody>
+              Are You Sure You Want To Delete This Item?
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" onClick={() => del(selectedItemId, selectedItemIndex)}>
+                Yes
+            </Button>{' '}
+            <Button color="primary" onClick={toggle}>
+                No
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Notes</th>
+              <th>Submitted By:</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {formState && formState.Customer_Notes
+              ? formState.Customer_Notes.reverse().map((i, index) => (
                 <tr key={index}>
-                  <td width={250}>{moment(i.date).format('MMMM Do YYYY, h:mm:ss a')}</td>
+                  <td width={250}>
+                    {moment(i.date).format('MMMM Do YYYY, h:mm:ss a')}
+                  </td>
                   <td>{i.note}</td>
                   <td>{i.Name}</td>
-                  <td><Button color="danger" onClick={() => (this.props.dispatch(arrayRemove('CustomerEdit', 'Customer_Notes', index)),deleteNote(i.id,formState, cookie))}>X</Button></td>
+                  <td>
+                    <Button
+                      color="danger"
+                      onClick={(e) => {
+                        toggle();
+                        setSelectedItemId(i.id);
+                        setSelectedItemIndex(index);
+                      }}
+                    >
+                          X
+                    </Button>
+                  </td>
                 </tr>
-              )) : null}
-            </tbody>
-          </Table>
-
-        </div>
-      );
-    } else {
-      return (
-        <div />
-      );
-    }
+              ))
+              : null}
+          </tbody>
+        </Table>
+      </div>
+    );
+  } else {
+    return <div />;
   }
-}
+};
 
 
 const mapStateToProps = (state, props) => ({
   formState: getFormValues('CustomerEdit')(state),
 });
 
-const mapDispatchToProps = dispatch =>
+const mapDispatchToProps = (dispatch) =>
   bindActionCreators(
     {
       change,
       updateNotes,
-      deleteNote
+      deleteNote,
     },
     dispatch
   );
@@ -69,8 +131,4 @@ Notes_Table = reduxForm({
   form: 'CustomerEdit',
 })(Notes_Table);
 
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Notes_Table);
+export default connect(mapStateToProps, mapDispatchToProps)(Notes_Table);
