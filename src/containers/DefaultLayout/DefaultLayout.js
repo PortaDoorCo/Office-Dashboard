45bd39238d561ef280b0b9ffba216d860f0ae256
heyjaypray <hey@jaypray.com>
2021-02-22 12:07:48 -0700

    add tour overlay

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -1,6 +1,7 @@
-import React, { Component, Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Container } from 'reactstrap';
+import Tour from 'reactour'
 
 import {
   AppAside,
@@ -56,76 +57,134 @@ const DefaultAside = React.lazy(() => import('./DefaultAside'));
 const DefaultFooter = React.lazy(() => import('./DefaultFooter'));
 const DefaultHeader = React.lazy(() => import('./DefaultHeader'));
 
-class DefaultLayout extends Component {
-  loading = () => (
+
+const steps = [
+  {
+    selector: '[data-tour="order-table"]',
+    content: 'Welcome to the Porta Door Dashboard',
+  },
+  {
+    selector: '.side-bar-tour',
+    content: 'Here you will be able to navigate to different parts of the app.  Click Door Order to create your first order.',
+  },
+  {
+    selector: '.job-info-tour',
+    content: 'Here is where you will select your customer, shipping method, due date and shipping address.',
+  },
+  {
+    selector: '.order-tour',
+    content: 'Here is where you enter the door info.  Select between a variety of options from woodtypes, designs, edges, etc.',
+  },
+  {
+    selector: '.misc-item-tour',
+    content: 'Here is where you will add Miscellaneous Items and submit your order.',
+  },
+  {
+    selector: '.side-bar-tour',
+    content: 'Click on Settings to view more settings',
+  },
+  {
+    selector: '.account-tour',
+    content: 'Here is where your account settings are.  Edit your name, email, and password.  It is generally good practice to change your password a few times a year.',
+  },
+  {
+    selector: '.account-tour',
+    content: 'Click on Doors in the Settings Navigation bar at the top and then Cope and Stick',
+    position: 'top'
+  },
+  {
+    selector: '.account-tour',
+    content: 'Here you can see all the attributes to the doors.  With elevated credentials you will be able to edit things such as pricing, breakdowns, and other attributes',
+    position: 'top'
+  },
+  {
+    selector: '.app',
+    content: 'Now please enjoy :) -  Feel free to contact me on slack or by email Justin@portadoor.com',
+    position: 'top'
+  },
+
+  // ...
+];
+
+
+let DefaultLayout = (props) => {
+  let loading = () => (
     <div className="animated fadeIn pt-1 text-center">
       <div className="sk-spinner sk-spinner-pulse"></div>
     </div>
   );
 
-  render() {
-    const { customerDBLoaded } = this.props;
+  const [isTourOpen, setIsTourOpen] = useState(true);
 
-    if (!customerDBLoaded) {
-      return (
-        <div>
-          <Loader />
-        </div>
-      );
-    } else {
-      return (
-        <div className="app">
-          <NotificationContainer />
-          <AppHeader fixed>
-            <Suspense fallback={this.loading()}>
-              <DefaultHeader />
+
+  const { customerDBLoaded } = props;
+
+  if (!customerDBLoaded) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  } else {
+    return (
+      <div className="app">
+        <Tour
+          steps={steps}
+          isOpen={isTourOpen}
+          onRequestClose={() => setIsTourOpen(false)}
+          updateDelay={5}
+        />
+        <NotificationContainer />
+        <AppHeader fixed>
+          <Suspense fallback={loading()}>
+            <DefaultHeader />
+          </Suspense>
+        </AppHeader>
+        <div className="app-body">
+          <AppSidebar fixed display="lg" className="side-bar-tour">
+            <AppSidebarHeader />
+            <AppSidebarForm />
+            <Suspense>
+              <AppSidebarNav navConfig={navigation} {...props} />
             </Suspense>
-          </AppHeader>
-          <div className="app-body">
-            <AppSidebar fixed display="lg">
-              <AppSidebarHeader />
-              <AppSidebarForm />
-              <Suspense>
-                <AppSidebarNav navConfig={navigation} {...this.props} />
+            <AppSidebarFooter />
+            <AppSidebarMinimizer />
+          </AppSidebar>
+          <main className="main">
+            <AppBreadcrumb appRoutes={routes} />
+            <Container fluid>
+              <Suspense fallback={loading()}>
+                <Switch>
+                  {routes.map((route, idx) => {
+                    return route.component ? (
+                      <Route
+                        key={idx}
+                        path={route.path}
+                        exact={route.exact}
+                        name={route.name}
+                        render={(props) => <route.component {...props} />}
+                      />
+
+                    ) : null;
+                  })}
+                  <Redirect from="/" to="/dashboard" />
+                </Switch>
               </Suspense>
-              <AppSidebarFooter />
-              <AppSidebarMinimizer />
-            </AppSidebar>
-            <main className="main">
-              <AppBreadcrumb appRoutes={routes} />
-              <Container fluid>
-                <Suspense fallback={this.loading()}>
-                  <Switch>
-                    {routes.map((route, idx) => {
-                      return route.component ? (
-                        <Route
-                          key={idx}
-                          path={route.path}
-                          exact={route.exact}
-                          name={route.name}
-                          render={(props) => <route.component {...props} />}
-                        />
-                      ) : null;
-                    })}
-                    <Redirect from="/" to="/dashboard" />
-                  </Switch>
-                </Suspense>
-              </Container>
-            </main>
-            <AppAside fixed>
-              <Suspense fallback={this.loading()}>
-                <DefaultAside />
-              </Suspense>
-            </AppAside>
-          </div>
-          <AppFooter>
-            <Suspense fallback={this.loading()}>
-              <DefaultFooter />
+            </Container>
+          </main>
+          <AppAside fixed>
+            <Suspense fallback={loading()}>
+              <DefaultAside />
             </Suspense>
-          </AppFooter>
+          </AppAside>
         </div>
-      );
-    }
+        <AppFooter>
+          <Suspense fallback={loading()}>
+            <DefaultFooter />
+          </Suspense>
+        </AppFooter>
+      </div>
+    );
   }
 }
 
