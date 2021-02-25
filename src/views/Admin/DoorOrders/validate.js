102306e92f239df195b58ef40f17e6672817d487
heyjaypray <hey@jaypray.com>
2021-02-25 11:30:58 -0700

    update validation

--- src/views/Admin/DoorOrders/validate.js
+++ src/views/Admin/DoorOrders/validate.js
@@ -4,10 +4,16 @@ const validate = values => {
   const partArrayErrors = [];
   values && values.part_list && values.part_list.forEach((part, memberIndex) => {
     const partListErrors = {};
-    if (!part && !part.dimensions && !part.dimensions.length < 1) {
+
+    console.log(!part || !part.dimensions || !part.dimensions.length < 1);
+    if (!part || !part.dimensions || !part.dimensions.length) {
+
+      console.log('HEREEEE');  
       partListErrors.dimensions = 'Required';
       partArrayErrors[memberIndex] = partListErrors;
     }
+
+    console.log({partListErrors});
   });
   if (partArrayErrors.length) {
     errors.part_list = partArrayErrors;
