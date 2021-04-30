74c915da56216b3c4d277777891b96e7238fb649
heyjaypray <hey@jaypray.com>
2021-04-30 08:34:37 -0700

    update

--- src/views/Admin/Orders/PrintOuts/Sorting/GlassCheck.js
+++ src/views/Admin/Orders/PrintOuts/Sorting/GlassCheck.js
@@ -3,12 +3,38 @@ const GlassCheck = (i) => {
     .map((i, v) => {
       return `glass_check_${i}`;
     });
+
+  const checker = arr => arr.every(v => v === true);
   const glassPanels = a.map(j => i.hasOwnProperty(j));
-  const isGlass = glassPanels.includes(true);
-  return {
-    ...i,
-    isGlass,
-  };
+  const hasGlass = glassPanels.includes(true);
+  const multiGlass = glassPanels.length > 1 ? !checker(glassPanels) : false;
+
+  console.log({multiGlass});
+
+  if(hasGlass && !multiGlass){
+    return {
+      ...i,
+      hasGlass,
+      multiGlass,
+      glass_index: 1
+    };
+  } else if (hasGlass && multiGlass){
+    return {
+      ...i,
+      hasGlass,
+      multiGlass,
+      glass_index: 2
+    };
+  } else {
+    return {
+      ...i,
+      hasGlass,
+      multiGlass,
+      glass_index: 3
+    };
+  }
+
+
 };
 
 export default GlassCheck;
