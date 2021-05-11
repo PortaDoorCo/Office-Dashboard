06de3ff3ad65e52f79a370066a0987fb715d6650
heyjaypray <hey@jaypray.com>
2021-05-11 10:10:01 -0700

    update db

--- src/redux/db_url.js
+++ src/redux/db_url.js
@@ -1,3 +1,3 @@
-// const db_url = 'https://server.portadoor.com'; 
-const db_url = 'http://localhost:1337';
+const db_url = 'https://server.portadoor.com'; 
+// const db_url = 'http://localhost:1337';
 export default db_url;
