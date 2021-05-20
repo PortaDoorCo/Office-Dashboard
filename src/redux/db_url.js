30b6950d8294444929f08912de9b3748293e13e2
heyjaypray <hey@jaypray.com>
2021-05-20 16:23:18 -0700

    add archive url

--- src/redux/db_url.js
+++ src/redux/db_url.js
@@ -1,3 +1,4 @@
-const db_url = 'https://server.portadoor.com'; 
+// const db_url = 'https://server.portadoor.com'; 
+const db_url = 'https://portadoor-server-v1.herokuapp.com/';
 // const db_url = 'http://localhost:1337';
 export default db_url;
