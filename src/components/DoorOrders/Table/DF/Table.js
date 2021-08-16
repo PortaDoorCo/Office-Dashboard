601bde6650113205f346f9df15c7273d6201900d
heyjaypray <hey@jaypray.com>
2021-08-16 13:21:57 -0400

    add default framing sizes

--- src/components/DoorOrders/Table/DF/Table.js
+++ src/components/DoorOrders/Table/DF/Table.js
@@ -187,8 +187,10 @@ const Cope_Table = ({
   const addFields = (i) => {
 
     const construction = formState?.part_list[i]?.construction?.value;
-    const profile = formState?.part_list[i]?.profile?.PROFILE_WIDTH;
-    const design = formState?.part_list[i]?.design?.PROFILE_WIDTH;
+    const leftStile = formState?.part_list[i]?.leftStile;
+    const rightStile = formState?.part_list[i]?.rightStile;
+    const topRail = formState?.part_list[i]?.topRail;
+    const bottomRail = formState?.part_list[i]?.bottomRail;
 
     const index = fields.length - 1;
 
@@ -265,15 +267,7 @@ const Cope_Table = ({
       startAsyncValidation('DoorOrder')
     );
 
-    let df_reduction = 0;
 
-    if(construction === 'Cope'){
-      df_reduction = formState?.part_list[i]?.profile?.DF_Reduction;
-    } else if (construction === 'MT'){
-      df_reduction = formState?.part_list[i]?.design?.DF_REDUCTION;
-    } else {
-      df_reduction = design;
-    }
 
     fields.push({
       qty: 1,
@@ -281,20 +275,16 @@ const Cope_Table = ({
       panelsW: 1,
       leftStile: leftStileWidth
         ? fraction(numQty(leftStileWidth))
-        : (construction === 'Cope' && profile) ?
-          fraction(profile) :
-          fraction(design),
+        : leftStile,
       rightStile: rightStileWidth
         ? fraction(numQty(rightStileWidth))
-        : (construction === 'Cope' && profile) ?
-          fraction(profile) :
-          fraction(design) ,
+        : rightStile,
       topRail: topRailWidth
         ? fraction(numQty(topRailWidth))
-        : fraction(df_reduction) ,
+        : topRail,
       bottomRail: bottomRailWidth
         ? fraction(numQty(bottomRailWidth))
-        : fraction(df_reduction) ,
+        : bottomRail,
       horizontalMidRailSize: 0,
       verticalMidRailSize: 0,
       unevenSplitInput: '0',
