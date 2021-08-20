ffcf7759f3f92fd9627ddbeea26c392e51a9b910
heyjaypray <hey@jaypray.com>
2021-08-20 12:18:55 -0400

    add last updated

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import axios from 'axios';
+
 
 const propTypes = {
   children: PropTypes.node,
@@ -9,14 +11,33 @@ const propTypes = {
 const defaultProps = {};
 
 class DefaultFooter extends Component {
+
+  state = {
+    repoInfo: []
+  }
+
+  componentDidMount(){
+    axios
+      .get('https://api.github.com/orgs/portadoorco/repos')
+      .then(response => {
+        this.setState({ repoInfo: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
   render() {
 
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
+    const { repoInfo } = this.state;
+
+    console.log({repo: this.state.repoInfo});
 
     return (
       <React.Fragment>
-        <span> &copy; {moment().format('YYYY')} <a href="https://portadoor.com">Porta Door Co. Inc.</a> - Version 0.9.0</span>
+        <span> &copy; {moment().format('YYYY')} <a href="https://portadoor.com">Porta Door Co. Inc.</a> - Last Updated: {moment(repoInfo[0]?.pushed_at).format('ddd MMM D YYYY h:mm a')}</span>
         <span className="ml-auto">Developed by <a href="https://thinkthoughtmedia.com" target="_blank" rel="noopener noreferrer">Think Thought Media LLC.</a></span>
       </React.Fragment>
     );
