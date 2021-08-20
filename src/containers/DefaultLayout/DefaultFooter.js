4681b1fe9e348eb1d85c3ef2766a0e4cc90b79df
heyjaypray <hey@jaypray.com>
2021-08-20 15:36:48 -0400

    update release date

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import axios from 'axios';
+import env from 'react-dotenv';
 
 
 const propTypes = {
@@ -12,30 +13,16 @@ const defaultProps = {};
 
 class DefaultFooter extends Component {
 
-  state = {
-    last_updated: 'Loading'
-  }
-
-  componentDidMount(){
-    axios
-      .get('https://api.github.com/repos/PortaDoorCo/Office-Dashboard/branches/master')
-      .then(response => {
-        this.setState({ last_updated: response?.data?.commit?.commit?.committer?.date});
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
 
   render() {
 
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
-    const { last_updated } = this.state;
+
 
     return (
       <React.Fragment>
-        <span> &copy; {moment().format('YYYY')} <a href="https://portadoor.com">Porta Door Co. Inc.</a> - Version: {moment(last_updated).format('YY.M.D.hhmm')}</span>
+        <span> &copy; {moment().format('YYYY')} <a href="https://portadoor.com">Porta Door Co. Inc.</a> - Version: {moment(env.HEROKU_RELEASE_CREATED_AT).format('YY.M.D.hhmm')}</span>
         <span className="ml-auto">Developed by <a href="https://thinkthoughtmedia.com" target="_blank" rel="noopener noreferrer">Think Thought Media LLC.</a></span>
       </React.Fragment>
     );
