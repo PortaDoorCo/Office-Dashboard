8636448255b511a57dde78e4f79e7fe2dba0fbd5
heyjaypray <hey@jaypray.com>
2021-09-30 14:07:03 -0700

    update status'

--- src/redux/customers/actions.js
+++ src/redux/customers/actions.js
@@ -15,119 +15,136 @@ export const CUSTOMER_UPDATED = 'CUSTOMER_UPDATED';
 export const CUSTOMER_DELETED = 'CUSTOMER_DELETED';
 export const UPLOAD_FILE_TO_CUSTOMER = 'UPLOAD_FILE_TO_CUSTOMER';
 
-
 export function setSelectedCompanies(data) {
-
   return async function (dispatch) {
     return await dispatch({
       type: SET_SELECTED_COMPANY,
-      data: data
+      data: data,
     });
   };
 }
 
 export function loadCustomers(cookie, user) {
+  console.log({ user });
 
-  console.log({user});
-
-  if(user.role?.type === 'customer'){
+  if (user.role?.type === 'customer') {
     return async function (dispatch) {
-      const res = await fetch(`${db_url}/companyprofiles/${user.company.id}`,
-        {
-          headers: {
-            'Authorization': `Bearer ${cookie}`
-          }
-        }
-      );
+      const res = await fetch(`${db_url}/companyprofiles/${user.company.id}`, {
+        headers: {
+          Authorization: `Bearer ${cookie}`,
+        },
+      });
       const data = await res.json();
       await dispatch(hideLoading());
       return await dispatch({
         type: LOAD_CUSTOMERS,
-        data: [data]
-      }
-      );
+        data: [data],
+      });
+    };
+  } else if (user.role?.type === 'quality_control') {
+    return async function (dispatch) {
+      const res = await fetch(`${db_url}/companyprofiles/1`, {
+        headers: {
+          Authorization: `Bearer ${cookie}`,
+        },
+      });
+      const data = await res.json();
+      await dispatch(hideLoading());
+      return await dispatch({
+        type: LOAD_CUSTOMERS,
+        data: [data],
+      });
     };
   } else {
     return async function (dispatch) {
-      const res = await fetch(`${db_url}/companyprofiles?_sort=id:ASC&_limit=50`,
+      const res = await fetch(
+        `${db_url}/companyprofiles?_sort=id:ASC&_limit=50`,
         {
           headers: {
-            'Authorization': `Bearer ${cookie}`
-          }
+            Authorization: `Bearer ${cookie}`,
+          },
         }
       );
       const data = await res.json();
       await dispatch(hideLoading());
       return await dispatch({
         type: LOAD_CUSTOMERS,
-        data: data
-      }
-      );
+        data: data,
+      });
     };
   }
-
 }
 
 export function loadAllCustomers(cookie, user) {
-
-  if(user.role?.type === 'customer'){
+  if (user.role?.type === 'customer') {
     return async function (dispatch) {
       await dispatch(hideLoading());
       return await dispatch({
         type: LOAD_ALL_CUSTOMERS,
-      }
-      );
+      });
+    };
+  } else if (user.role?.type === 'quality_control') {
+    return async function (dispatch) {
+      await dispatch(hideLoading());
+      return await dispatch({
+        type: LOAD_ALL_CUSTOMERS,
+      });
     };
   } else {
     return async function (dispatch) {
-      const res = await fetch(`${db_url}/companyprofiles/all`,
-        {
-          headers: {
-            'Authorization': `Bearer ${cookie}`
-          }
-        }
-      );
+      const res = await fetch(`${db_url}/companyprofiles/all`, {
+        headers: {
+          Authorization: `Bearer ${cookie}`,
+        },
+      });
       const data = await res.json();
       await dispatch(hideLoading());
       return await dispatch({
         type: LOAD_ALL_CUSTOMERS,
-        data: data
-      }
-      );
+        data: data,
+      });
     };
   }
-
 }
 
 export function dbNotLoaded(cookie) {
   return async function (dispatch) {
     return await dispatch({
       type: DB_NOT_LOADED,
-    }
-    );
+    });
   };
 }
 
 export function updateCustomer(custId, customer, cookie) {
   return async function (dispatch) {
     try {
-      const res = await axios.put(`${db_url}/companyprofiles/${custId}`, customer,
+      const res = await axios.put(
+        `${db_url}/companyprofiles/${custId}`,
+        customer,
         {
           headers: {
-            'Authorization': `Bearer ${cookie}`
-          }
+            Authorization: `Bearer ${cookie}`,
+          },
         }
       );
       const data = await res;
 
-      NotificationManager.success('Customer has been update!', 'Customer Updated!', 2000);
+      NotificationManager.success(
+        'Customer has been update!',
+        'Customer Updated!',
+        2000
+      );
       return dispatch({
         type: UPDATE_CUSTOMER,
-        data: data.data
+        data: data.data,
       });
     } catch (error) {
       console.error(error);
-      NotificationManager.error('There was an problem with your submission', 'Error', 2000);
+      NotificationManager.error(
+        'There was an problem with your submission',
+        'Error',
+        2000
+      );
     }
   };
 }
@@ -146,11 +163,15 @@ export function uploadFilesToCustomer(customer, e, cookie) {
 
   return async function (dispatch) {
     try {
-      const res = await axios.put(`${db_url}/companyprofiles/${orderId}`, files, {
-        headers: {
-          Authorization: `Bearer ${cookie}`,
-        },
-      });
+      const res = await axios.put(
+        `${db_url}/companyprofiles/${orderId}`,
+        files,
+        {
+          headers: {
+            Authorization: `Bearer ${cookie}`,
+          },
+        }
+      );
       const data = await res;
 
       return dispatch({
@@ -173,79 +194,90 @@ export function submitCustomer(customer, cookie) {
     try {
       const res = await axios.post(`${db_url}/companyprofiles`, customer, {
         headers: {
-          'Authorization': `Bearer ${cookie}`
-        }
+          Authorization: `Bearer ${cookie}`,
+        },
       });
 
-
-      NotificationManager.success('Customer has been added!', 'Submission Succeeded!', 2000);
+      NotificationManager.success(
+        'Customer has been added!',
+        'Submission Succeeded!',
+        2000
+      );
       return dispatch({
         type: SUBMIT_CUSTOMER,
-        data: res
+        data: res,
       });
     } catch (error) {
       console.error(error);
-      NotificationManager.error('There was an problem with your submission', 'Error', 2000);
+      NotificationManager.error(
+        'There was an problem with your submission',
+        'Error',
+        2000
+      );
     }
   };
 }
 
-
 export function updateNotes(orderId, data, cookie) {
-
   const item = {
     Customer_Notes: [
       ...data.Customer_Notes,
       {
-        'id': data.id,
-        'note': data.note,
-        'date': new Date(),
-        'Name': data.Name
-      }
-    ]
+        id: data.id,
+        note: data.note,
+        date: new Date(),
+        Name: data.Name,
+      },
+    ],
   };
 
   return async function (dispatch) {
     try {
       await axios.put(`${db_url}/companyprofiles/${orderId}`, item, {
         headers: {
-          'Authorization': `Bearer ${cookie}`
-        }
+          Authorization: `Bearer ${cookie}`,
+        },
       });
       return dispatch({
         type: UPDATE_NOTES,
       });
     } catch (error) {
       console.error(error);
-      NotificationManager.error('There was an problem with your submission', 'Error', 2000);
+      NotificationManager.error(
+        'There was an problem with your submission',
+        'Error',
+        2000
+      );
     }
   };
 }
 
-
 export function deleteNote(id, data, cookie) {
-
   const orderId = data.id;
 
   const item = {
-    Customer_Notes: data.Customer_Notes.filter(x => {
+    Customer_Notes: data.Customer_Notes.filter((x) => {
       return x.id !== id;
-    })
+    }),
   };
 
   return async function (dispatch) {
     try {
       await axios.put(`${db_url}/companyprofiles/${orderId}`, item, {
         headers: {
-          'Authorization': `Bearer ${cookie}`
-        }
+          Authorization: `Bearer ${cookie}`,
+        },
       });
       return dispatch({
         type: UPDATE_NOTES,
       });
     } catch (error) {
       console.error(error);
-      NotificationManager.error('There was an problem with your submission', 'Error', 2000);
+      NotificationManager.error(
+        'There was an problem with your submission',
+        'Error',
+        2000
+      );
     }
   };
 }
@@ -276,6 +308,3 @@ export function customerDeleted(res) {
     });
   };
 }
-
-
-
