b5330adb6c459826c186946b57f231dd9aecf245
heyjaypray <hey@jaypray.com>
2022-01-13 15:54:38 -0500

    update management

--- src/components/DoorOrders/Table/Face_Frame/Face_Frame_Table.js
+++ src/components/DoorOrders/Table/Face_Frame/Face_Frame_Table.js
@@ -346,7 +346,7 @@ const Frame_Only_Table = ({
               {role?.type === 'authenticated' ||
               role?.type === 'owner' ||
               role?.type === 'administrator' ||
-              
+              role?.type === 'management' ||
               role?.type === 'office' ? (
                   <Col xs="3">
                     <strong>Extra Design Cost</strong>
