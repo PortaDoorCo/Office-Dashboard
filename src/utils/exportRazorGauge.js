861d347700bc01874d02b465c1eb99a44d9438c9
heyjaypray <hey@jaypray.com>
2022-01-19 10:17:08 -0500

    format

--- src/utils/exportRazorGauge.js
+++ src/utils/exportRazorGauge.js
@@ -1,8 +1,8 @@
-import numQty from 'numeric-quantity';
-import axios from 'axios';
-import { NotificationManager } from 'react-notifications';
-import Stiles from '../views/PrintOuts/Breakdowns/Doors/Stiles/Stiles';
-import Rails from '../views/PrintOuts/Breakdowns/Doors/Rails/Rails';
+import numQty from "numeric-quantity";
+import axios from "axios";
+import { NotificationManager } from "react-notifications";
+import Stiles from "../views/PrintOuts/Breakdowns/Doors/Stiles/Stiles";
+import Rails from "../views/PrintOuts/Breakdowns/Doors/Rails/Rails";
 
 const exportThis = (data, breakdowns) => {
   console.log(data);
@@ -39,112 +39,117 @@ const exportThis = (data, breakdowns) => {
 
     const razor = itemNumCounter
       ? itemNumCounter.part_list?.map((f, index) => {
-      // console.log({ f });
+          // console.log({ f });
 
-        console.log({f});
+          console.log({ f });
 
-        if(f.construction?.value !== 'Slab'){
-          f.dimensions.forEach((j, ind) => {
-            // console.log({ j });
-  
-            const stile = (Stiles(j, f, breakdowns) || []).map((rail) => {
-              return rail;
-            });
-  
-            const rail = (Rails(j, f, breakdowns) || []).map((rail) => {
-              return rail;
-            });
-  
-            // console.log({ rail });
-            // console.log({ stile });
-  
-            const stilePrint = stile.map((i) => {
-              return razorGauge.push([
-                `${d.orderNum}`,
-                `${f.woodtype?.NAME} ${f.thickness?.thickness_1}`,
-                Math.round(numQty(i.width) * 16) / 16,
-                Math.round(numQty(i.height) * 16) / 16,
-                i.qty_2,
-                i.razor_pattern,
-                `${f.design?.NAME} ${f.thickness?.thickness_1}`,
-                i.item,
+          if (f.construction?.value !== "Slab") {
+            f.dimensions.forEach((j, ind) => {
+              // console.log({ j });
+
+              const stile = (Stiles(j, f, breakdowns) || []).map((rail) => {
+                return rail;
+              });
+
+              const rail = (Rails(j, f, breakdowns) || []).map((rail) => {
+                return rail;
+              });
+
+              // console.log({ rail });
+              // console.log({ stile });
+
+              const stilePrint = stile.map((i) => {
+                return razorGauge.push([
+                  `${d.orderNum}`,
+                  `${f.woodtype?.NAME} ${f.thickness?.thickness_1}`,
+                  Math.round(numQty(i.width) * 16) / 16,
+                  Math.round(numQty(i.height) * 16) / 16,
+                  i.qty_2,
+                  i.razor_pattern,
+                  `${f.design?.NAME} ${f.thickness?.thickness_1}`,
+                  i.item,
                   f.profile?.NAME
                     ? f.profile?.NAME
                     : f.design?.NAME
-                      ? f.design?.NAME
-                      : '',
-              ]);
-            });
-  
-            const railPrint = rail.map((i) => {
-              console.log({ i });
-              return razorGauge.push([
-                `${d.orderNum}`,
-                `${f.woodtype?.NAME} ${f.thickness?.thickness_1}`,
-                Math.round(numQty(i.width) * 16) / 16,
-                Math.round(numQty(i.height) * 16) / 16,
-                i.qty_2,
-                i.razor_pattern,
-                `${f.design?.NAME} ${f.thickness?.thickness_1}`,
-                i.item,
+                    ? f.design?.NAME
+                    : "",
+                ]);
+              });
+
+              const railPrint = rail.map((i) => {
+                console.log({ i });
+                return razorGauge.push([
+                  `${d.orderNum}`,
+                  `${f.woodtype?.NAME} ${f.thickness?.thickness_1}`,
+                  Math.round(numQty(i.width) * 16) / 16,
+                  Math.round(numQty(i.height) * 16) / 16,
+                  i.qty_2,
+                  i.razor_pattern,
+                  `${f.design?.NAME} ${f.thickness?.thickness_1}`,
+                  i.item,
                   f.profile?.NAME
                     ? f.profile?.NAME
                     : f.design?.NAME
-                      ? f.design?.NAME
-                      : '',
-              ]);
+                    ? f.design?.NAME
+                    : "",
+                ]);
+              });
             });
-          });
-        }
+          }
 
-
-        return razorGauge;
-      })
+          return razorGauge;
+        })
       : [];
 
-    const token = 'D-8j9sffu8sAAAAAAAAAAemdC1XQBd05yzxnMcrWQS035ekpJ2hxb2T-SRun9TD9';
+    const token =
+      "D-8j9sffu8sAAAAAAAAAAemdC1XQBd05yzxnMcrWQS035ekpJ2hxb2T-SRun9TD9";
 
-    console.log({razorGauge});
+    console.log({ razorGauge });
 
-    let csvContent = razorGauge.map(e => e.join(',')).join('\n');
+    let csvContent = razorGauge.map((e) => e.join(",")).join("\n");
 
     let myParams = {
-      'path': `/Razorgauge/${d.orderNum}.csv`,
-      'mode': 'add',
-      'autorename': true,
-      'mute': false,
-      'strict_conflict': false
+      path: `/Razorgauge/${d.orderNum}.csv`,
+      mode: "add",
+      autorename: true,
+      mute: false,
+      strict_conflict: false,
     };
 
     try {
-      const f = await axios.post('https://content.dropboxapi.com/2/files/upload', csvContent,
+      const f = await axios.post(
+        "https://content.dropboxapi.com/2/files/upload",
+        csvContent,
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/octet-stream',
-            'Dropbox-API-Arg': JSON.stringify(myParams)
+            "Content-Type": "application/octet-stream",
+            "Dropbox-API-Arg": JSON.stringify(myParams),
           },
         }
       );
-      console.log('dddddddd==>>', f);
+      console.log("dddddddd==>>", f);
 
-      NotificationManager.success(`#${d.orderNum} Razor Gauge Successfully Exported!`, 'Success', 2000);
-
-
-    } catch(err) {
-      console.log('errrrrr==>>', err);
-      NotificationManager.error('There was an problem with your upload', 'Error', 2000);
+      NotificationManager.success(
+        `#${d.orderNum} Razor Gauge Successfully Exported!`,
+        "Success",
+        2000
+      );
+    } catch (err) {
+      console.log("errrrrr==>>", err);
+      NotificationManager.error(
+        "There was an problem with your upload",
+        "Error",
+        2000
+      );
     }
 
     // console.log({newItem});
 
     return razorGauge;
-
   });
 
-  console.log({newData});
-
-
+  console.log({ newData });
 
   return newData;
 };
