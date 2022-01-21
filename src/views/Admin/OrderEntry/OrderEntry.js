9f43c89aa6cdda4708f7b9d1f46f41cad0f1e1cf
heyjaypray <hey@jaypray.com>
2022-01-21 08:21:40 -0500

    update

--- src/views/Admin/OrderEntry/OrderEntry.js
+++ src/views/Admin/OrderEntry/OrderEntry.js
@@ -648,13 +648,12 @@ const mapStateToProps = (state, props) => ({
           },
           salesRep: state.Orders?.selectedOrder?.job_info?.salesRep
             ? state.Orders?.selectedOrder?.job_info?.salesRep
-            : {
-              fullName: state.sales?.salesReps?.filter(
-                (x) =>
-                  x.id ===
+            : state.sales?.salesReps?.filter(
+              (x) =>
+                x.id ===
                       state.Orders?.selectedOrder?.job_info?.customer?.sale
-              )[0],
-            },
+            )[0],
+            
           EMAIL: state.Orders?.selectedOrder?.job_info?.customer?.EMAIL,
           Email2: state.Orders?.selectedOrder?.job_info?.customer?.Email2,
           Email3: state.Orders?.selectedOrder?.job_info?.customer?.Email3,
