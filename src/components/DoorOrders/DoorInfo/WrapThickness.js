69b479eef965da2c4bed2e8c8c84faefaa796286
heyjaypray <hey@jaypray.com>
2022-06-03 13:57:52 -0400

    add wrap thickness

---
+++ src/components/DoorOrders/DoorInfo/WrapThickness.js
@@ -0,0 +1,40 @@
+const thickness = [
+  {
+    name: '4/4 Standard Grade',
+    thickness_1: '4/4',
+    thickness_2: '3/4',
+    db_name: 'STANDARD_GRADE',
+    grade_name: '',
+    value: 1,
+    thickness_values: 0.75,
+  },
+  {
+    name: '4/4 Select Grade',
+    thickness_1: '4/4',
+    thickness_2: '3/4',
+    db_name: 'SELECT_GRADE',
+    grade_name: 'Select ',
+    value: 2,
+    thickness_values: 0.75,
+  },
+  {
+    name: '5/4 Standard Grade',
+    thickness_1: '5/4',
+    thickness_2: '1',
+    db_name: 'STANDARD_GRADE_THICK',
+    grade_name: '',
+    value: 3,
+    thickness_values: 1,
+  },
+  {
+    name: '5/4 Select Grade',
+    thickness_1: '5/4',
+    thickness_2: '1',
+    db_name: 'SELECT_GRADE_THICK',
+    grade_name: 'Select ',
+    value: 4,
+    thickness_values: 1,
+  },
+];
+
+export default thickness;
