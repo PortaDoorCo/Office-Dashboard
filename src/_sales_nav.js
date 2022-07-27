60b6dc3811f26126811b99f654bed3f45299a17b
heyjaypray <hey@jaypray.com>
2022-07-27 10:08:45 -0400

    update sales page

--- src/_sales_nav.js
+++ src/_sales_nav.js
@@ -3,44 +3,43 @@ export default {
     {
       title: true,
       name: 'Dashboard',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      wrapper: {
+        // optional wrapper object
+        element: '', // required valid HTML5 element tag
+        attributes: {}, // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
       },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
+      class: '', // optional class names space delimited list for title item ex: "text-center"
     },
     {
       name: 'Dashboard',
       url: '/dashboard',
       icon: 'dashboard',
-
     },
     {
       title: true,
       name: '',
       icon: 'icon-speedometer',
-
     },
-    {
-      name: 'Door Order',
-      url: '/door-order',
-    },
-    {
-      name: 'Drawer Order',
-      url: '/drawer-order',
-    },
-    {
-      name: 'Face Frames',
-      url: '/face-frames',
-    },
-    {
-      name: 'Mouldings',
-      url: '/mouldings',
-    },
-    {
-      name: 'Misc. Order',
-      url: '/misc-items',
-    },
+    // {
+    //   name: 'Door Order',
+    //   url: '/door-order',
+    // },
+    // {
+    //   name: 'Drawer Order',
+    //   url: '/drawer-order',
+    // },
+    // {
+    //   name: 'Face Frames',
+    //   url: '/face-frames',
+    // },
+    // {
+    //   name: 'Mouldings',
+    //   url: '/mouldings',
+    // },
+    // {
+    //   name: 'Misc. Order',
+    //   url: '/misc-items',
+    // },
     {
       name: 'Reports',
       icon: '',
@@ -53,13 +52,12 @@ export default {
         //   name: 'Tracking',
         //   url: '/tracking',
         // },
-      ]
+      ],
     },
 
     {
       name: 'Customers',
       url: '/customers',
-
     },
     {
       name: 'Settings',
@@ -70,11 +68,9 @@ export default {
       url: '/catalog',
     },
 
-
     // {
     //   name: 'Calendar',
     //   url: '/calendar',
     // },
-
-  ]
+  ],
 };
