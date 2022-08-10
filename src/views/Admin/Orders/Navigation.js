4c4016dd8ee75b2cdbb6b8429c0fdf27f7521246
heyjaypray <hey@jaypray.com>
2022-08-10 16:38:55 -0400

    update sales order icons

--- src/views/Admin/Orders/Navigation.js
+++ src/views/Admin/Orders/Navigation.js
@@ -12,37 +12,45 @@ import Attachment from '@material-ui/icons/Attachment';
 const Navigation = (props) => {
   console.log({ props });
 
-  if (
-    props.user?.role?.type !== 'quality_control' &&
-    props.user?.role?.type !== 'sales'
-  ) {
+  if (props.user?.role?.type !== 'quality_control') {
     return (
       <Col>
-        <Tooltip title="Edit" placement="top">
-          <IconButton onClick={props.editable}>
-            <Edit style={{ width: '40', height: '40' }} />
-          </IconButton>
-        </Tooltip>
+        {props.user?.role?.type !== 'sales' ? (
+          <Tooltip title="Edit" placement="top">
+            <IconButton onClick={props.editable}>
+              <Edit style={{ width: '40', height: '40' }} />
+            </IconButton>
+          </Tooltip>
+        ) : null}
+
         <Tooltip title="Tracking History" placement="top">
           <IconButton onClick={props.toggleTracking}>
             <List style={{ width: '40', height: '40' }} />
           </IconButton>
         </Tooltip>
-        <Tooltip title="Balance" placement="top">
-          <IconButton onClick={props.toggleBalance}>
-            <AttachMoneyIcon style={{ width: '40', height: '40' }} />
-          </IconButton>
-        </Tooltip>
+
+        {props.user?.role?.type !== 'sales' ? (
+          <Tooltip title="Balance" placement="top">
+            <IconButton onClick={props.toggleBalance}>
+              <AttachMoneyIcon style={{ width: '40', height: '40' }} />
+            </IconButton>
+          </Tooltip>
+        ) : null}
+
         <Tooltip title="Misc Items" placement="top">
           <IconButton onClick={props.toggleMiscItems}>
             <Dns style={{ width: '40', height: '40' }} />
           </IconButton>
         </Tooltip>
-        <Tooltip title="View Notes" placement="top">
-          <IconButton onClick={props.toggleNotes}>
-            <Chat style={{ width: '40', height: '40' }} />
-          </IconButton>
-        </Tooltip>
+
+        {props.user?.role?.type !== 'sales' ? (
+          <Tooltip title="View Notes" placement="top">
+            <IconButton onClick={props.toggleNotes}>
+              <Chat style={{ width: '40', height: '40' }} />
+            </IconButton>
+          </Tooltip>
+        ) : null}
+
         {!props.edit ? (
           <Tooltip title="View Files" placement="top">
             <IconButton onClick={props.toggleFiles}>
