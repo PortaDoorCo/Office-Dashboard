e62ec31e2e67ad65950fad1f55624aac6e843b46
heyjaypray <hey@jaypray.com>
2023-02-28 21:28:07 -0700

    change profile notes

--- src/components/DoorOrders/DoorInfo/Functions/changeProfile.js
+++ src/components/DoorOrders/DoorInfo/Functions/changeProfile.js
@@ -6,6 +6,19 @@ const changeProfile = (p, ind, props, change) => {
   const part = formState.part_list[ind];
 
   if (part?.orderType?.value === 'DF') {
+    const topRail = part.profile
+      ? part.profile.DF_Reduction
+        ? part.profile.DF_Reduction
+        : part.profile.PROFILE_WIDTH
+      : 0;
+    const bottomRail = part.profile
+      ? part.profile.DF_Reduction
+        ? part.profile.DF_Reduction
+        : part.profile.PROFILE_WIDTH
+      : 0;
+    const leftStile = part.profile.PROFILE_WIDTH;
+    const rightStile = part.profile.PROFILE_WIDTH;
+
     if (part.profile?.PROFILE_WIDTH > 0) {
       props.dispatch(
         change(
@@ -50,8 +63,24 @@ const changeProfile = (p, ind, props, change) => {
           )
         )
       );
+
+      props.dispatch(
+        change(
+          'Order',
+          `part_list[${ind}].notes`,
+          `Left Stile: ${fraction(leftStile)}" Right Stile: ${fraction(
+            rightStile
+          )}" \nTop Rail: ${fraction(topRail)}" Bottom Rail: ${fraction(
+            bottomRail
+          )}"`
+        )
+      );
     }
   } else {
+    const topRail = part.profile.PROFILE_WIDTH + part.design?.TOP_RAIL_ADD;
+    const bottomRail = part.profile.PROFILE_WIDTH + part.design?.BTM_RAIL_ADD;
+    const leftStile = part.profile.PROFILE_WIDTH;
+    const rightStile = part.profile.PROFILE_WIDTH;
     if (part.profile?.PROFILE_WIDTH > 0) {
       props.dispatch(
         change(
@@ -90,6 +119,18 @@ const changeProfile = (p, ind, props, change) => {
               ? part.profile.PROFILE_WIDTH + part.design?.BTM_RAIL_ADD
               : 0
           )
+        )
+      );
+
+      props.dispatch(
+        change(
+          'Order',
+          `part_list[${ind}].notes`,
+          `Left Stile: ${fraction(leftStile)}" Right Stile: ${fraction(
+            rightStile
+          )}" \nTop Rail: ${fraction(topRail)}" Bottom Rail: ${fraction(
+            bottomRail
+          )}"`
         )
       );
     }
