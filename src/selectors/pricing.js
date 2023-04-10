e9319a8b4ff3b3be03a49c5c8653288d3cfa883c
heyjaypray <hey@jaypray.com>
2023-04-10 15:16:32 -0400

    fix wrapped pricing

--- src/selectors/pricing.js
+++ src/selectors/pricing.js
@@ -991,22 +991,36 @@ export const linePriceSelector = createSelector(
                 ((parseInt(i.panelsH) === 1 && numQty(i.height) >= 48) ||
                   (parseInt(i.panelsW) === 1 && numQty(i.width) >= 24))
               ) {
-                if (
-                  !order?.oldPricing &&
-                  moment(order?.created_at) > moment('11-08-2022')
-                ) {
-                  // console.log('less');
-                  const k = currency(item[index][p]).multiply(1.2).value;
-                  const base = currency(k).multiply(parseInt(i.qty)).value;
-                  const price = base;
-                  return currency(price).value;
+                if (part?.construction?.value === 'Wrapped') {
+                  if (moment(order?.created_at) > moment('04-10-2023')) {
+                    const k = currency(item[index][p]).value;
+                    const base = currency(k).multiply(parseInt(i.qty)).value;
+                    const price = base;
+                    return currency(price).value;
+                  } else {
+                    const k = currency(item[index][p]).multiply(1.2).value;
+                    const base = currency(k).multiply(parseInt(i.qty)).value;
+                    const price = base;
+                    return currency(price).value;
+                  }
                 } else {
-                  // console.log('more');
-                  const base =
-                    Math.round(item[index][p] * parseInt(i.qty) * 100) / 100;
-                  const add = base * 0.2;
-                  const price = base + add;
-                  return Math.round(price * 100) / 100;
+                  if (
+                    !order?.oldPricing &&
+                    moment(order?.created_at) > moment('11-08-2022')
+                  ) {
+                    // console.log('less');
+                    const k = currency(item[index][p]).multiply(1.2).value;
+                    const base = currency(k).multiply(parseInt(i.qty)).value;
+                    const price = base;
+                    return currency(price).value;
+                  } else {
+                    // console.log('more');
+                    const base =
+                      Math.round(item[index][p] * parseInt(i.qty) * 100) / 100;
+                    const add = base * 0.2;
+                    const price = base + add;
+                    return Math.round(price * 100) / 100;
+                  }
                 }
               } else {
                 if (part?.orderType?.value === 'Face_Frame') {
