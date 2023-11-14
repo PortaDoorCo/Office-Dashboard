fae14339fbd16a0b51879ccdb54528c0cb71c355
heyjaypray <hey@jaypray.com>
2023-11-14 11:29:17 -0700

    update

--- src/views/PrintOuts/Breakdowns/Doors/Panels/designs/One_Piece_Door/One_Piece_Door.js
+++ src/views/PrintOuts/Breakdowns/Doors/Panels/designs/One_Piece_Door/One_Piece_Door.js
@@ -2,38 +2,28 @@ import numQty from 'numeric-quantity';
 import Ratio from 'lb-ratio';
 // import frac2dec from '../frac2dec'
 
-const fraction = num => {
+const fraction = (num) => {
   let fraction = Ratio.parse(num).toQuantityOf(2, 3, 4, 8, 16);
   return fraction.toLocaleString();
 };
 
 export default (info) => {
-
   const height = numQty(info.height);
   const width = numQty(info.width);
   const qty = parseInt(info.qty);
-  
+
   const door = [
     {
-      qty: `(${qty})`,
-      measurement: `${fraction(
-        Math.round((
-          width)
-                    * 16) / 16
-      )} x ${fraction(
-        Math.round((
-          height)
-                    * 16) / 16
-      )}`,
+      qty: ``,
+      measurement: 'OP',
       pattern: '',
       width: 0,
       height: 0,
       panel: '',
       count: qty,
-      multiplier: qty
+      multiplier: qty,
     },
   ];
 
   return door;
-
 };
