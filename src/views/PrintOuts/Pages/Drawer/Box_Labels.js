520b18b849dc0c654c2e8e72332adacb91315057
Justin <hey@jaypray.com>
2024-09-11 08:36:00 -0700

    update

--- src/views/PrintOuts/Pages/Drawer/Box_Labels.js
+++ src/views/PrintOuts/Pages/Drawer/Box_Labels.js
@@ -30,7 +30,7 @@ const DrawerPDF = async (data, breakdowns, p, pricing) => {
       pageSize: 'LETTER',
       pageOrientation: 'portrait',
       content: ContentSorted,
-      pageMargins: [20, 47, 20, 40], // Slightly smaller margins
+      pageMargins: [20, 47, 20, 10], // Slightly smaller margins
 
       pageBreakBefore: function (
         currentNode,
